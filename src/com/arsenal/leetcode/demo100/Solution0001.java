a5c7eb8a4241b662688046d20f612f1102f5da76
Arsenal <funing.ma.china@gmail.com>
2020-05-01 12:24:43 +0800

    add leetcode problem 0001

--- src/com/arsenal/leetcode/demo100/Solution0001.java
+++ src/com/arsenal/leetcode/demo100/Solution0001.java
@@ -1,6 +1,8 @@
 package com.arsenal.leetcode.demo100;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Vector;
 
 /**
@@ -16,30 +18,50 @@ import java.util.Vector;
 public class Solution0001 {
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
-        int target = 9;
-        int[] subIndex = twoSum(nums, target);
+        int target = 17;
+        //int[] subIndex = twoSum(nums, target);
+        int[] subIndex = optimizeTwoSum(nums, target);
         System.out.println(Arrays.toString(subIndex));
     }
 
     /**
      * 大佬的解决方案效率高，时间复杂度O(n)
      * https://leetcode.com/problems/two-sum/discuss/3/Accepted-Java-O(n)-Solution
+     * <p>
+     * 复杂度分析：
+     * <p>
+     * 时间复杂度：O(n)，
+     * 我们只遍历了包含有n个元素的列表一次。在表中进行的每次查找只花费 O(1) 的时间。
+     * <p>
+     * 空间复杂度：O(n)，
+     * 所需的额外空间取决于哈希表中存储的元素数量，该表最多需要存储n个元素。
      * @param nums   数组
      * @param target 和
      * @return 下标数组
      */
     public static int[] optimizeTwoSum(int[] nums, int target) {
+        if (nums.length < 3) {
+            return null;
+        }
 
-        return null;
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[1] = i;
+                result[0] = map.get(target - nums[i]);
+            }
+            map.put(nums[i], i);
+        }
+        return result;
     }
 
     /**
      * 我的方法比较容易理解，但是效率较低，时间复杂度O(n^2)
-     *
+     * <p>
      * 时间复杂度：O(n^2)
      * 对于每个元素，我们试图通过遍历数组的其余部分来寻找它所对应的目标元素，这将耗费 O(n) 的时间。因此时间复杂度为 O(n2)。
      * 空间复杂度：O(1)。
-     *
      * @param nums   数组
      * @param target 和
      * @return 下标数组
