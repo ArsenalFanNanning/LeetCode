0bd166acd647a1ba2cfb16ca6170e5d6755348a5
Arsenal <funing.ma.china@gmail.com>
2021-08-29 23:02:34 +0800

    add leetcode problem 0005

--- src/com/arsenal/leetcode/demo100/Solution0005.java
+++ src/com/arsenal/leetcode/demo100/Solution0005.java
@@ -11,6 +11,7 @@ public class Solution0005 {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
+        System.out.println("Hello World");
         System.out.println("Hello Java");
         System.out.println("Hello Java");
         System.out.println("Hello Java");
