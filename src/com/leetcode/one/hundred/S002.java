5eb219dd7b8c0e92c6cfbe086156271d102637b3
Arsenal <funing.ma.china@gmail.com>
2022-11-23 00:38:43 +0800

    002 两数相加

--- src/com/leetcode/one/hundred/S002.java
+++ src/com/leetcode/one/hundred/S002.java
@@ -41,11 +41,29 @@ package com.leetcode.one.hundred;
  */
 public class S002 {
     public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        ListNode l12 = new ListNode(4);
+        ListNode l13 = new ListNode(3);
+        l1.next = l12;
+        l12.next = l13;
 
+        ListNode l2 = new ListNode(5);
+        ListNode l22 = new ListNode(6);
+        ListNode l23 = new ListNode(4);
+        l2.next = l22;
+        l22.next = l23;
+
+        ListNode listNode = addTwoNumbers(l1, l2);
+        ListNode cur = listNode;
+        while (cur != null) {
+            System.out.println(cur.val);
+            cur = cur.next;
+        }
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+        ListNode cur = l1;
+        return cur;
     }
 }
 
@@ -63,5 +81,13 @@ class ListNode {
     ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
     }
 }
