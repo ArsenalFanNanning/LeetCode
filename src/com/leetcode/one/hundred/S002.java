5c87488a9b60b4041d04d1188ba8af10988a9e7a
Arsenal <funing.ma.china@gmail.com>
2022-11-24 00:50:21 +0800

    002 两数相加

--- src/com/leetcode/one/hundred/S002.java
+++ src/com/leetcode/one/hundred/S002.java
@@ -68,6 +68,7 @@ public class S002 {
         l22.next = l23;
         l23.next = l24;
 
+        //ListNode listNode = violence(l1, l2);
         ListNode listNode = addTwoNumbers(l1, l2);
         ListNode cur = listNode;
         while (cur != null) {
@@ -77,6 +78,33 @@ public class S002 {
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+
+    public static ListNode violence(ListNode l1, ListNode l2) {
         ListNode cur = l1;
         String num1 = "";
         while (cur != null) {
