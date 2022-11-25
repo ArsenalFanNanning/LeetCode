c6840cf410afff86b4ba7a3e32bd527cada8604a
Arsenal <funing.ma.china@gmail.com>
2022-11-25 23:52:51 +0800

    005 最长回文子串

--- src/com/leetcode/one/hundred/S005.java
+++ src/com/leetcode/one/hundred/S005.java
@@ -15,6 +15,21 @@ package com.leetcode.one.hundred;
  * 提示：
  * 1 <= s.length <= 1000
  * s 仅由数字和英文字母组成
+ * 5. Longest Palindromic Substring
+ * Given a string s, return the longest
+ * palindromic
+ * substring
+ * in s.
+ * Example 1:
+ * Input: s = "babad"
+ * Output: "bab"
+ * Explanation: "aba" is also a valid answer.
+ * Example 2:
+ * Input: s = "cbbd"
+ * Output: "bb"
+ * Constraints:
+ * 1 <= s.length <= 1000
+ * s consist of only digits and English letters.
  */
 public class S005 {
     public static void main(String[] args) {
