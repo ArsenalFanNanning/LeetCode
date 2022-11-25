24b44df21f0d7dfc1aef2c00828fc0048cdc7525
Arsenal <funing.ma.china@gmail.com>
2022-11-25 23:52:10 +0800

    005 最长回文子串

---
+++ src/com/leetcode/one/hundred/S005.java
@@ -0,0 +1,27 @@
+package com.leetcode.one.hundred;
+
+/**
+ * @Author: Arsenal
+ * @Date: 2022-11-25 23:51
+ * @Description: 最长回文子串-中等
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ * 示例 1：
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ * 示例 2：
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ * 提示：
+ * 1 <= s.length <= 1000
+ * s 仅由数字和英文字母组成
+ */
+public class S005 {
+    public static void main(String[] args) {
+
+    }
+
+    public String longestPalindrome(String s) {
+        return "";
+    }
+}
