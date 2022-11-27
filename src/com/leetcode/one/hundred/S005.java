616eabeb97384f93313bf1c86edd68efabecea5d
Arsenal <funing.ma.china@gmail.com>
2022-11-27 09:10:09 +0800

    005 最长回文子串

--- src/com/leetcode/one/hundred/S005.java
+++ src/com/leetcode/one/hundred/S005.java
@@ -33,9 +33,10 @@ package com.leetcode.one.hundred;
  */
 public class S005 {
     public static void main(String[] args) {
-        String s = "cbbd";
-       // System.out.println(violence(s));
-        System.out.println(longestPalindrome(s));
+        String s = "babad";
+        // System.out.println(violence(s));
+        System.out.println(optimize(s));
+       // System.out.println(longestPalindrome(s));
     }
 
     public static String longestPalindrome(String s) {
@@ -61,6 +62,42 @@ public class S005 {
             ++right;
         }
         return right - left - 1;
+    }
+
+    public static String optimize(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        //  保存起始位置，测试了用数组似乎能比全局变量稍快一点
+        int[] range = new int[2];
+        char[] str = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            // 把回文看成中间的部分全是同一字符，左右部分相对称
+            // 找到下一个与当前字符不同的字符
+            i = findLongest(str, i, range);
+        }
+        return s.substring(range[0], range[1] + 1);
+    }
+
+    public static int findLongest(char[] str, int low, int[] range) {
+        // 查找中间部分
+        int high = low;
+        while (high < str.length - 1 && str[high + 1] == str[low]) {
+            high++;
+        }
+        // 定位中间部分的最后一个字符
+        int ans = high;
+        // 从中间向左右扩散
+        while (low > 0 && high < str.length - 1 && str[low - 1] == str[high + 1]) {
+            low--;
+            high++;
+        }
+        // 记录最大长度
+        if (high - low > range[1] - range[0]) {
+            range[0] = low;
+            range[1] = high;
+        }
+        return ans;
     }
 
     public static String violence(String s) {
