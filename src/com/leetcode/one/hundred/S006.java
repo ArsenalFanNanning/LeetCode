ea32f58f6ab5493e428d48990ffb536e51a6e8bb
Arsenal <funing.ma.china@gmail.com>
2022-11-27 10:13:10 +0800

    006 Z字形变换

--- src/com/leetcode/one/hundred/S006.java
+++ src/com/leetcode/one/hundred/S006.java
@@ -1,5 +1,8 @@
 package com.leetcode.one.hundred;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: Arsenal
  * @Date: 2022-11-27 09:04
@@ -66,6 +69,17 @@ public class S006 {
     }
 
     public static String convert(String s, int numRows) {
-        return s;
+        if (numRows < 2) return s;
+        List<StringBuilder> rows = new ArrayList<StringBuilder>();
+        for (int i = 0; i < numRows; i++) rows.add(new StringBuilder());
+        int i = 0, flag = -1;
+        for (char c : s.toCharArray()) {
+            rows.get(i).append(c);
+            if (i == 0 || i == numRows - 1) flag = -flag;
+            i += flag;
+        }
+        StringBuilder res = new StringBuilder();
+        for (StringBuilder row : rows) res.append(row);
+        return res.toString();
     }
 }
