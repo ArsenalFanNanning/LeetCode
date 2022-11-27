196810a6bd2fafb7265b391439d236fb0aeed450
Arsenal <funing.ma.china@gmail.com>
2022-11-27 09:24:40 +0800

    006 Z字形变换

--- src/com/leetcode/one/hundred/S006.java
+++ src/com/leetcode/one/hundred/S006.java
@@ -60,6 +60,12 @@ package com.leetcode.one.hundred;
  */
 public class S006 {
     public static void main(String[] args) {
+        String s = "PAYPALISHIRING";
+        int numRows = 3;
+        System.out.println(convert(s, numRows));
+    }
 
+    public static String convert(String s, int numRows) {
+        return s;
     }
 }
