2a2b72ccd260b509d9c1fb45fbdb9c650d1fa8b8
Arsenal <funing.ma.china@gmail.com>
2022-11-29 00:29:00 +0800

    007 整数反转

--- src/com/leetcode/one/hundred/S007.java
+++ src/com/leetcode/one/hundred/S007.java
@@ -41,7 +41,27 @@ public class S007 {
     public static void main(String[] args) {
         int x = -2147483412;
         //System.out.println(violence(x));
-        System.out.println(reverse(x));
+        //System.out.println(reverse(x));
+        System.out.println(optimize(x));
+    }
+
+    private static int optimize(int x) {
+        int res = 0;
+        while (x != 0) {
+            //每次取末尾数字
+            int tmp = x % 10;
+            //判断是否 大于 最大32位整数
+            if (res > 214748364 || (res == 214748364 && tmp > 7)) {
+                return 0;
+            }
+            //判断是否 小于 最小32位整数
+            if (res < -214748364 || (res == -214748364 && tmp < -8)) {
+                return 0;
+            }
+            res = res * 10 + tmp;
+            x /= 10;
+        }
+        return res;
     }
 
     public static int reverse(int x) {
