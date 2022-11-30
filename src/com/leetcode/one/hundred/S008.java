2eee4184ccc39a1f31182423a268b6c5496bd64a
Arsenal <funing.ma.china@gmail.com>
2022-11-30 23:39:43 +0800

    008 字符串转换整数

--- src/com/leetcode/one/hundred/S008.java
+++ src/com/leetcode/one/hundred/S008.java
@@ -111,7 +111,8 @@ package com.leetcode.one.hundred;
  */
 public class S008 {
     public static void main(String[] args) {
-
+        String s = "   -42";
+        System.out.println(myAtoi(s));
     }
 
     public static int myAtoi(String s) {
