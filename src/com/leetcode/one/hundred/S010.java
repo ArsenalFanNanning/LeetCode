7fd5124c4955b92bc9b3bff713fda154a5f4c074
Arsenal <funing.ma.china@gmail.com>
2022-12-02 00:04:49 +0800

    010 正则表达式匹配

--- src/com/leetcode/one/hundred/S010.java
+++ src/com/leetcode/one/hundred/S010.java
@@ -26,6 +26,29 @@ package com.leetcode.one.hundred;
  * s 只包含从 a-z 的小写字母。
  * p 只包含从 a-z 的小写字母，以及字符 . 和 *。
  * 保证每次出现字符 * 时，前面都匹配到有效的字符
+ * 10. Regular Expression Matching
+ * Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+ * '.' Matches any single character.​​​​
+ * '*' Matches zero or more of the preceding element.
+ * The matching should cover the entire input string (not partial).
+ * Example 1:
+ * Input: s = "aa", p = "a"
+ * Output: false
+ * Explanation: "a" does not match the entire string "aa".
+ * Example 2:
+ * Input: s = "aa", p = "a*"
+ * Output: true
+ * Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+ * Example 3:
+ * Input: s = "ab", p = ".*"
+ * Output: true
+ * Explanation: ".*" means "zero or more (*) of any character (.)".
+ * Constraints:
+ * 1 <= s.length <= 20
+ * 1 <= p.length <= 30
+ * s contains only lowercase English letters.
+ * p contains only lowercase English letters, '.', and '*'.
+ * It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
  */
 public class S010 {
 }
