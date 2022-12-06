9630006fb394f2fc5133cd7f47f77b2fda7f979b
Arsenal <funing.ma.china@gmail.com>
2022-12-07 00:08:22 +0800

    013 罗马数字转整数

--- src/com/leetcode/one/hundred/S013.java
+++ src/com/leetcode/one/hundred/S013.java
@@ -81,4 +81,45 @@ package com.leetcode.one.hundred;
  * It is guaranteed that s is a valid roman numeral in the range [1, 3999].
  */
 public class S013 {
+    public static void main(String[] args) {
+        String s = "MCMXCIV";
+        System.out.println(romanToInt(s));
+    }
+
+    public static int romanToInt(String s) {
+        int sum = 0;
+        int preNum = getValue(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            int num = getValue(s.charAt(i));
+            if (preNum < num) {
+                sum -= preNum;
+            } else {
+                sum += preNum;
+            }
+            preNum = num;
+        }
+        sum += preNum;
+        return sum;
+    }
+
+    private static int getValue(char ch) {
+        switch (ch) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
 }
