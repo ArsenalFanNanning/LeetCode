f008e3606e6fde0d9d7be8cd4f8d6d08c3c7aa8e
Arsenal <funing.ma.china@gmail.com>
2022-12-06 23:48:03 +0800

    012 整数转罗马数字

--- src/com/leetcode/one/hundred/S012.java
+++ src/com/leetcode/one/hundred/S012.java
@@ -74,10 +74,30 @@ package com.leetcode.one.hundred;
 public class S012 {
     public static void main(String[] args) {
         int num = 1994;
+        //System.out.println(violence(num));
         System.out.println(intToRoman(num));
     }
 
     public static String intToRoman(int num) {
+        // 贪婪算法，类似于找零钱
+        // 把阿拉伯数字与罗马数字可能出现的所有情况和对应关系，放在两个数组中
+        // 并且按照阿拉伯数字的大小降序排列
+        int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int index = 0;
+        StringBuilder result = new StringBuilder();
+        while (index < 13) {
+            if (num >= nums[index]) {
+                result.append(romans[index]);
+                num -= nums[index];
+            } else {
+                index++;
+            }
+        }
+        return result.toString();
+    }
+
+    public static String violence(int num) {
         StringBuilder sb = new StringBuilder();
         while (num != 0) {
             int place = place(num);
