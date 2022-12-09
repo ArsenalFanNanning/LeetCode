833cba56b5d550a6e7fd8b7b97445b50c919977f
Arsenal <funing.ma.china@gmail.com>
2022-12-10 00:05:59 +0800

    017 电话号码的字母组合

--- src/com/leetcode/one/hundred/S017.java
+++ src/com/leetcode/one/hundred/S017.java
@@ -1,6 +1,8 @@
 package com.leetcode.one.hundred;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Author: Arsenal
@@ -44,6 +46,17 @@ public class S017 {
     }
 
     public static List<String> letterCombinations(String digits) {
+        Map<Character, String> phoneMap = new HashMap<Character, String>() {{
+            put('2', "abc");
+            put('3', "def");
+            put('4', "ghi");
+            put('5', "jkl");
+            put('6', "mno");
+            put('7', "pqrs");
+            put('8', "tuv");
+            put('9', "wxyz");
+        }};
+
         return null;
     }
 }
