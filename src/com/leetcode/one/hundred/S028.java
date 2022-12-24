c2d56c7e6d00cea4c3d7ad5975ad80b2243766b3
Arsenal <funing.ma.china@gmail.com>
2022-12-24 19:19:16 +0800

    028 找出字符串中第一个匹配项的下标

--- src/com/leetcode/one/hundred/S028.java
+++ src/com/leetcode/one/hundred/S028.java
@@ -36,10 +36,41 @@ public class S028 {
     public static void main(String[] args) {
         String haystack = "sadbutsad";
         String needle = "sad";
+        System.out.println(violence(haystack, needle));
         System.out.println(strStr(haystack, needle));
     }
 
     public static int strStr(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        if (m == 0) {
+            return 0;
+        }
+        int[] pi = new int[m];
+        for (int i = 1, j = 0; i < m; i++) {
+            while (j > 0 && needle.charAt(i) != needle.charAt(j)) {
+                j = pi[j - 1];
+            }
+            if (needle.charAt(i) == needle.charAt(j)) {
+                j++;
+            }
+            pi[i] = j;
+        }
+        for (int i = 0, j = 0; i < n; i++) {
+            while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
+                j = pi[j - 1];
+            }
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == m) {
+                return i - m + 1;
+            }
+        }
+        return -1;
+    }
+
+
+    public static int violence(String haystack, String needle) {
         int n = haystack.length(), m = needle.length();
         for (int i = 0; i + m <= n; i++) {
             boolean flag = true;
