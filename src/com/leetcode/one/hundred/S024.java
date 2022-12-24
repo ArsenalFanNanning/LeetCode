3d6571d112905f7d1d89ed1fbea638c48b135c82
Arsenal <funing.ma.china@gmail.com>
2022-12-24 18:59:00 +0800

    024 两两交换链表中的节点

--- src/com/leetcode/one/hundred/S024.java
+++ src/com/leetcode/one/hundred/S024.java
@@ -17,6 +17,21 @@ package com.leetcode.one.hundred;
  * 提示：
  * 链表中节点的数目在范围 [0, 100] 内
  * 0 <= Node.val <= 100
+ * 24. Swap Nodes in Pairs
+ * Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem
+ * without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+ * Example 1:
+ * Input: head = [1,2,3,4]
+ * Output: [2,1,4,3]
+ * Example 2:
+ * Input: head = []
+ * Output: []
+ * Example 3:
+ * Input: head = [1]
+ * Output: [1]
+ * Constraints:
+ * The number of nodes in the list is in the range [0, 100].
+ * 0 <= Node.val <= 100
  */
 public class S024 {
 }
