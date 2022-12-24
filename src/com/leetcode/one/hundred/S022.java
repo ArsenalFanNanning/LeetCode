45e086e28f0f073f050319ebc8c8eb7c4b241da1
Arsenal <funing.ma.china@gmail.com>
2022-12-24 18:46:39 +0800

    022 括号生成

--- src/com/leetcode/one/hundred/S022.java
+++ src/com/leetcode/one/hundred/S022.java
@@ -31,6 +31,30 @@ public class S022 {
     public static void main(String[] args) {
         int n = 3;
         System.out.println(violence(n));
+        System.out.println(generateParenthesis_backtrack(n));
+    }
+
+    public static List<String> generateParenthesis_backtrack(int n) {
+        List<String> ans = new ArrayList<String>();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+    public static void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            ans.add(cur.toString());
+            return;
+        }
+        if (open < max) {
+            cur.append('(');
+            backtrack(ans, cur, open + 1, close, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+        if (close < open) {
+            cur.append(')');
+            backtrack(ans, cur, open, close + 1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
     }
 
     public static List<String> violence(int n) {
