2352c92645bc8b321d57e7ac95b819175131d945
Arsenal <funing.ma.china@gmail.com>
2022-12-24 18:42:04 +0800

    021 合并两个有序链表

--- src/com/leetcode/one/hundred/S021.java
+++ src/com/leetcode/one/hundred/S021.java
@@ -61,9 +61,31 @@ public class S021 {
         l23.next = l24;
 
         System.out.println(mergeTwoLists(l1, l2));
+        //System.out.println(mergeTwoLists_recursion(l1, l2));
     }
 
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
+    }
+
+    public static ListNode mergeTwoLists_recursion(ListNode l1, ListNode l2) {
         if (l1 == null) {
             return l2;
         } else if (l2 == null) {
