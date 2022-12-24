e285d91a9d2fc475790562c0e64ed0fd4f9ac7ac
Arsenal <funing.ma.china@gmail.com>
2022-12-24 18:52:12 +0800

    023 合并K个升序链表

--- src/com/leetcode/one/hundred/S023.java
+++ src/com/leetcode/one/hundred/S023.java
@@ -30,6 +30,44 @@ package com.leetcode.one.hundred;
  * -10^4 <= lists[i][j] <= 10^4
  * lists[i] 按 升序 排列
  * lists[i].length 的总和不超过 10^4
+ * 23. Merge k Sorted Lists
+ * You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+ * Merge all the linked-lists into one sorted linked-list and return it.
+ * Example 1:
+ * Input: lists = [[1,4,5],[1,3,4],[2,6]]
+ * Output: [1,1,2,3,4,4,5,6]
+ * Explanation: The linked-lists are:
+ * [
+ * 1->4->5,
+ * 1->3->4,
+ * 2->6
+ * ]
+ * merging them into one sorted list:
+ * 1->1->2->3->4->4->5->6
+ * Example 2:
+ * Input: lists = []
+ * Output: []
+ * Example 3:
+ * Input: lists = [[]]
+ * Output: []
+ * Constraints:
+ * k == lists.length
+ * 0 <= k <= 104
+ * 0 <= lists[i].length <= 500
+ * -104 <= lists[i][j] <= 104
+ * lists[i] is sorted in ascending order.
+ * The sum of lists[i].length will not exceed 104.
  */
 public class S023 {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(9);
+        ListNode l2 = new ListNode(8);
+        ListNode l3 = new ListNode(7);
+        ListNode[] lists = {l1, l2, l3};
+        System.out.println(mergeKLists(lists));
+    }
+
+    public static ListNode mergeKLists(ListNode[] lists) {
+        return null;
+    }
 }
