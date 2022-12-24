0ff7d2e42b731564b81722f101b9a2685a60c84f
Arsenal <funing.ma.china@gmail.com>
2022-12-24 18:45:42 +0800

    022 括号生成

--- src/com/leetcode/one/hundred/S022.java
+++ src/com/leetcode/one/hundred/S022.java
@@ -1,5 +1,6 @@
 package com.leetcode.one.hundred;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -29,10 +30,41 @@ import java.util.List;
 public class S022 {
     public static void main(String[] args) {
         int n = 3;
-        System.out.println(generateParenthesis(n));
+        System.out.println(violence(n));
     }
 
-    public static List<String> generateParenthesis(int n) {
-        return null;
+    public static List<String> violence(int n) {
+        List<String> combinations = new ArrayList<String>();
+        generateAll(new char[2 * n], 0, combinations);
+        return combinations;
     }
+
+    public static void generateAll(char[] current, int pos, List<String> result) {
+        if (pos == current.length) {
+            if (valid(current)) {
+                result.add(new String(current));
+            }
+        } else {
+            current[pos] = '(';
+            generateAll(current, pos + 1, result);
+            current[pos] = ')';
+            generateAll(current, pos + 1, result);
+        }
+    }
+
+    public static boolean valid(char[] current) {
+        int balance = 0;
+        for (char c : current) {
+            if (c == '(') {
+                ++balance;
+            } else {
+                --balance;
+            }
+            if (balance < 0) {
+                return false;
+            }
+        }
+        return balance == 0;
+    }
+
 }
