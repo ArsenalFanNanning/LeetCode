3613a4270b623e766a475c599386125d2dddf16d
Arsenal <funing.ma.china@gmail.com>
2022-12-24 19:18:35 +0800

    028 找出字符串中第一个匹配项的下标

--- src/com/leetcode/one/hundred/S028.java
+++ src/com/leetcode/one/hundred/S028.java
@@ -40,6 +40,19 @@ public class S028 {
     }
 
     public static int strStr(String haystack, String needle) {
-        return 0;
+        int n = haystack.length(), m = needle.length();
+        for (int i = 0; i + m <= n; i++) {
+            boolean flag = true;
+            for (int j = 0; j < m; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
