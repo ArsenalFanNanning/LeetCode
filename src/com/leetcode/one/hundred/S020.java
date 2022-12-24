7697e5fe02284b14239cf67aba49f9671e8d6c2d
Arsenal <funing.ma.china@gmail.com>
2022-12-24 18:32:19 +0800

    020 有效的括号

--- src/com/leetcode/one/hundred/S020.java
+++ src/com/leetcode/one/hundred/S020.java
@@ -1,5 +1,10 @@
 package com.leetcode.one.hundred;
 
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 /**
  * @Author: Arsenal
  * @Date: 2022-12-15 00:00
@@ -48,6 +53,28 @@ public class S020 {
     }
 
     public static boolean isValid(String s) {
-        return true;
+        int n = s.length();
+        if (n % 2 == 1) {
+            return false;
+        }
+
+        Map<Character, Character> pairs = new HashMap<Character, Character>() {{
+            put(')', '(');
+            put(']', '[');
+            put('}', '{');
+        }};
+        Deque<Character> stack = new LinkedList<Character>();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (pairs.containsKey(ch)) {
+                if (stack.isEmpty() || stack.peek() != pairs.get(ch)) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
     }
 }
