34351010d78450939b70d77da020b758a10e84e3
Arsenal <funing.ma.china@gmail.com>
2022-12-24 19:01:20 +0800

    024 两两交换链表中的节点

--- src/com/leetcode/one/hundred/S024.java
+++ src/com/leetcode/one/hundred/S024.java
@@ -48,9 +48,27 @@ public class S024 {
         l14.next = l15;
         l15.next = l16;
         l16.next = l17;
+        System.out.println(violence(l1));
+        System.out.println(swapPairs(l1));
     }
 
     public static ListNode swapPairs(ListNode head) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1;
+        }
+        return dummyHead.next;
+    }
+
+
+    public static ListNode violence(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
