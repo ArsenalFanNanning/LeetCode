b4c54f684aeeb468e490aa6589eb8ad60d661d5f
Arsenal <funing.ma.china@gmail.com>
2022-12-24 19:04:58 +0800

    025 K 个一组翻转链表

--- src/com/leetcode/one/hundred/S025.java
+++ src/com/leetcode/one/hundred/S025.java
@@ -34,4 +34,60 @@ package com.leetcode.one.hundred;
  * 0 <= Node.val <= 1000
  */
 public class S025 {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(8);
+        ListNode l12 = new ListNode(7);
+        ListNode l13 = new ListNode(5);
+        ListNode l14 = new ListNode(6);
+        ListNode l15 = new ListNode(4);
+        ListNode l16 = new ListNode(2);
+        ListNode l17 = new ListNode(3);
+        l1.next = l12;
+        l12.next = l13;
+        l13.next = l14;
+        l14.next = l15;
+        l15.next = l16;
+        l16.next = l17;
+        System.out.println(reverseKGroup(l1, 2));
+    }
+
+    public static ListNode reverseKGroup(ListNode head, int k) {
+        ListNode hair = new ListNode(0);
+        hair.next = head;
+        ListNode pre = hair;
+
+        while (head != null) {
+            ListNode tail = pre;
+            // 查看剩余部分长度是否大于等于 k
+            for (int i = 0; i < k; ++i) {
+                tail = tail.next;
+                if (tail == null) {
+                    return hair.next;
+                }
+            }
+            ListNode nex = tail.next;
+            ListNode[] reverse = myReverse(head, tail);
+            head = reverse[0];
+            tail = reverse[1];
+            // 把子链表重新接回原链表
+            pre.next = head;
+            tail.next = nex;
+            pre = tail;
+            head = tail.next;
+        }
+
+        return hair.next;
+    }
+
+    public static ListNode[] myReverse(ListNode head, ListNode tail) {
+        ListNode prev = tail.next;
+        ListNode p = head;
+        while (prev != tail) {
+            ListNode nex = p.next;
+            p.next = prev;
+            prev = p;
+            p = nex;
+        }
+        return new ListNode[]{tail, head};
+    }
 }
