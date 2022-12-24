7d4776e3ec1ba0118d313ddc7160d95d37aa7b25
Arsenal <funing.ma.china@gmail.com>
2022-12-24 18:40:55 +0800

    021 合并两个有序链表

--- src/com/leetcode/one/hundred/S021.java
+++ src/com/leetcode/one/hundred/S021.java
@@ -59,9 +59,21 @@ public class S021 {
         l2.next = l22;
         l22.next = l23;
         l23.next = l24;
+
+        System.out.println(mergeTwoLists(l1, l2));
     }
 
-    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        return list1;
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
     }
 }
