7d3266e94a9350a5ac09d4ec0263c66e02fb7f5b
Arsenal <funing.ma.china@gmail.com>
2022-12-24 19:24:23 +0800

    029 两数相除

--- src/com/leetcode/one/hundred/S029.java
+++ src/com/leetcode/one/hundred/S029.java
@@ -1,5 +1,8 @@
 package com.leetcode.one.hundred;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: Arsenal
  * @Date: 2022-12-24 19:19
@@ -41,10 +44,61 @@ public class S029 {
     public static void main(String[] args) {
         int dividend = 10;
         int divisor = 3;
+        System.out.println(divideBinary(dividend, divisor));
         System.out.println(divide(dividend, divisor));
     }
 
     public static int divide(int dividend, int divisor) {
+        // 考虑被除数为最小值的情况
+        if (dividend == Integer.MIN_VALUE) {
+            if (divisor == 1) {
+                return Integer.MIN_VALUE;
+            }
+            if (divisor == -1) {
+                return Integer.MAX_VALUE;
+            }
+        }
+        // 考虑除数为最小值的情况
+        if (divisor == Integer.MIN_VALUE) {
+            return dividend == Integer.MIN_VALUE ? 1 : 0;
+        }
+        // 考虑被除数为 0 的情况
+        if (dividend == 0) {
+            return 0;
+        }
+
+        // 一般情况，使用类二分查找
+        // 将所有的正数取相反数，这样就只需要考虑一种情况
+        boolean rev = false;
+        if (dividend > 0) {
+            dividend = -dividend;
+            rev = !rev;
+        }
+        if (divisor > 0) {
+            divisor = -divisor;
+            rev = !rev;
+        }
+
+        List<Integer> candidates = new ArrayList<Integer>();
+        candidates.add(divisor);
+        int index = 0;
+        // 注意溢出
+        while (candidates.get(index) >= dividend - candidates.get(index)) {
+            candidates.add(candidates.get(index) + candidates.get(index));
+            ++index;
+        }
+        int ans = 0;
+        for (int i = candidates.size() - 1; i >= 0; --i) {
+            if (candidates.get(i) >= dividend) {
+                ans += 1 << i;
+                dividend -= candidates.get(i);
+            }
+        }
+
+        return rev ? -ans : ans;
+    }
+
+    public static int divideBinary(int dividend, int divisor) {
         // 考虑被除数为最小值的情况
         if (dividend == Integer.MIN_VALUE) {
             if (divisor == 1) {
