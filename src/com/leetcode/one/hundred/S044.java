fa7fbb8b4235bf59999f3fcec090992fd17d70b7
Arsenal <funing.ma.china@gmail.com>
2022-12-25 19:49:20 +0800

    044 通配符匹配

--- src/com/leetcode/one/hundred/S044.java
+++ src/com/leetcode/one/hundred/S044.java
@@ -67,6 +67,7 @@ public class S044 {
         String s = "aa";
         String p = "*";
         System.out.println(isMatchDp(s, p));
+        System.out.println(isMatchGreedy(s, p));
     }
 
     public static boolean isMatchDp(String s, String p) {
@@ -91,6 +92,57 @@ public class S044 {
             }
         }
         return dp[m][n];
+    }
+
+    public static boolean isMatchGreedy(String s, String p) {
+        int sRight = s.length(), pRight = p.length();
+        while (sRight > 0 && pRight > 0 && p.charAt(pRight - 1) != '*') {
+            if (charMatch(s.charAt(sRight - 1), p.charAt(pRight - 1))) {
+                --sRight;
+                --pRight;
+            } else {
+                return false;
+            }
+        }
+
+        if (pRight == 0) {
+            return sRight == 0;
+        }
+
+        int sIndex = 0, pIndex = 0;
+        int sRecord = -1, pRecord = -1;
+
+        while (sIndex < sRight && pIndex < pRight) {
+            if (p.charAt(pIndex) == '*') {
+                ++pIndex;
+                sRecord = sIndex;
+                pRecord = pIndex;
+            } else if (charMatch(s.charAt(sIndex), p.charAt(pIndex))) {
+                ++sIndex;
+                ++pIndex;
+            } else if (sRecord != -1 && sRecord + 1 < sRight) {
+                ++sRecord;
+                sIndex = sRecord;
+                pIndex = pRecord;
+            } else {
+                return false;
+            }
+        }
+
+        return allStars(p, pIndex, pRight);
+    }
+
+    public static boolean allStars(String str, int left, int right) {
+        for (int i = left; i < right; ++i) {
+            if (str.charAt(i) != '*') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean charMatch(char u, char v) {
+        return u == v || v == '?';
     }
 
 }
