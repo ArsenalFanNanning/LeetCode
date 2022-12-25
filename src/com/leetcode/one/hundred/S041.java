068e005383466709f75243b5662d19902ca2bab6
Arsenal <funing.ma.china@gmail.com>
2022-12-25 19:36:32 +0800

    041 缺失的第一个正数

--- src/com/leetcode/one/hundred/S041.java
+++ src/com/leetcode/one/hundred/S041.java
@@ -38,4 +38,29 @@ package com.leetcode.one.hundred;
  * -231 <= nums[i] <= 231 - 1
  */
 public class S041 {
+    public static void main(String[] args) {
+        int[] nums = {7, 8, 9, 11, 12};
+        System.out.println(firstMissingPositive(nums));
+    }
+
+    public static int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int num = Math.abs(nums[i]);
+            if (num <= n) {
+                nums[num - 1] = -Math.abs(nums[num - 1]);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
 }
