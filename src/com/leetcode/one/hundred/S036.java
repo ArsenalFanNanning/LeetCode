69c9f7f2a0b5e9458c2535430a3d37705c987d31
Arsenal <funing.ma.china@gmail.com>
2022-12-25 19:03:01 +0800

    036 有效的数独

--- src/com/leetcode/one/hundred/S036.java
+++ src/com/leetcode/one/hundred/S036.java
@@ -1,5 +1,10 @@
 package com.leetcode.one.hundred;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * @Author: Arsenal
  * @Date: 2022-12-25 18:55
@@ -92,6 +97,7 @@ public class S036 {
                 {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
                 {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
         System.out.println(isValidSudoku(board));
+        System.out.println(isValidSudokuMap(board));
     }
 
     public static boolean isValidSudoku(char[][] board) {
@@ -110,6 +116,28 @@ public class S036 {
                         return false;
                     }
                 }
+            }
+        }
+        return true;
+    }
+
+    public static boolean isValidSudokuMap(char[][] board) {
+        Map<Integer, Set<Integer>> row  = new HashMap<>(), col = new HashMap<>(), area = new HashMap<>();
+        for (int i = 0; i < 9; i++) {
+            row.put(i, new HashSet<>());
+            col.put(i, new HashSet<>());
+            area.put(i, new HashSet<>());
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c == '.') continue;
+                int u = c - '0';
+                int idx = i / 3 * 3 + j / 3;
+                if (row.get(i).contains(u) || col.get(j).contains(u) || area.get(idx).contains(u)) return false;
+                row.get(i).add(u);
+                col.get(j).add(u);
+                area.get(idx).add(u);
             }
         }
         return true;
