37bcd0273eeabab8faceffc2b8361499cb5f58f7
Arsenal <funing.ma.china@gmail.com>
2022-12-25 19:52:24 +0800

    045 跳跃游戏 II

--- src/com/leetcode/one/hundred/S045.java
+++ src/com/leetcode/one/hundred/S045.java
@@ -38,4 +38,24 @@ package com.leetcode.one.hundred;
  * 0 <= nums[i] <= 1000
  */
 public class S045 {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 0, 1, 4};
+        System.out.println(jump(nums));
+    }
+
+    public static int jump(int[] nums) {
+        int position = nums.length - 1;
+        int steps = 0;
+        while (position > 0) {
+            for (int i = 0; i < position; i++) {
+                if (i + nums[i] >= position) {
+                    position = i;
+                    steps++;
+                    break;
+                }
+            }
+        }
+        return steps;
+    }
+
 }
