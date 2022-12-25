dfe15221d815b79af62b5b569643708934d000e3
Arsenal <funing.ma.china@gmail.com>
2022-12-25 19:31:39 +0800

    040 组合总和 II

--- src/com/leetcode/one/hundred/S040.java
+++ src/com/leetcode/one/hundred/S040.java
@@ -1,5 +1,9 @@
 package com.leetcode.one.hundred;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @Author: Arsenal
  * @Date: 2022-12-25 19:29
@@ -30,6 +34,47 @@ package com.leetcode.one.hundred;
  */
 public class S040 {
     public static void main(String[] args) {
+        int[] candidates = {10, 1, 2, 7, 6, 1, 5};
+        int target = 8;
+        System.out.println(combinationSum2(candidates, target));
+    }
 
+    static List<int[]> freq = new ArrayList<int[]>();
+    static List<List<Integer>> ans = new ArrayList<List<Integer>>();
+    static List<Integer> sequence = new ArrayList<Integer>();
+
+    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        for (int num : candidates) {
+            int size = freq.size();
+            if (freq.isEmpty() || num != freq.get(size - 1)[0]) {
+                freq.add(new int[]{num, 1});
+            } else {
+                ++freq.get(size - 1)[1];
+            }
+        }
+        dfs(0, target);
+        return ans;
+    }
+
+    public static void dfs(int pos, int rest) {
+        if (rest == 0) {
+            ans.add(new ArrayList<Integer>(sequence));
+            return;
+        }
+        if (pos == freq.size() || rest < freq.get(pos)[0]) {
+            return;
+        }
+
+        dfs(pos + 1, rest);
+
+        int most = Math.min(rest / freq.get(pos)[0], freq.get(pos)[1]);
+        for (int i = 1; i <= most; ++i) {
+            sequence.add(freq.get(pos)[0]);
+            dfs(pos + 1, rest - i * freq.get(pos)[0]);
+        }
+        for (int i = 1; i <= most; ++i) {
+            sequence.remove(sequence.size() - 1);
+        }
     }
 }
