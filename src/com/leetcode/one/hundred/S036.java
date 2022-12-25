808eb5dba4c883e8b059dfdd6110ac1e2567596e
Arsenal <funing.ma.china@gmail.com>
2022-12-25 19:04:23 +0800

    036 有效的数独

--- src/com/leetcode/one/hundred/S036.java
+++ src/com/leetcode/one/hundred/S036.java
@@ -99,6 +99,7 @@ public class S036 {
         System.out.println(isValidSudoku(board));
         System.out.println(isValidSudokuMap(board));
         System.out.println(isValidSudokuArray(board));
+        System.out.println(isValidSudokuOpt(board));
     }
 
     public static boolean isValidSudoku(char[][] board) {
@@ -154,6 +155,24 @@ public class S036 {
                 int idx = i / 3 * 3 + j / 3;
                 if (row[i][u] || col[j][u] || area[idx][u]) return false;
                 row[i][u] = col[j][u] = area[idx][u] = true;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isValidSudokuOpt(char[][] board) {
+        int[] row = new int[10], col = new int[10], area = new int[10];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c == '.') continue;
+                int u = c - '0';
+                int idx = i / 3 * 3 + j / 3;
+                if ((((row[i] >> u) & 1) == 1) || (((col[j] >> u) & 1) == 1) || (((area[idx] >> u) & 1) == 1))
+                    return false;
+                row[i] |= (1 << u);
+                col[j] |= (1 << u);
+                area[idx] |= (1 << u);
             }
         }
         return true;
