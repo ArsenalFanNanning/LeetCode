61c6b9d784080922973c97a0ee7bfef2040a4e24
Arsenal <funing.ma.china@gmail.com>
2022-12-26 23:28:24 +0800

    046 全排列

--- src/com/leetcode/one/hundred/S046.java
+++ src/com/leetcode/one/hundred/S046.java
@@ -18,6 +18,21 @@ package com.leetcode.one.hundred;
  * 1 <= nums.length <= 6
  * -10 <= nums[i] <= 10
  * nums 中的所有整数 互不相同
+ * 46. Permutations
+ * Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+ * Example 1:
+ * Input: nums = [1,2,3]
+ * Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * Example 2:
+ * Input: nums = [0,1]
+ * Output: [[0,1],[1,0]]
+ * Example 3:
+ * Input: nums = [1]
+ * Output: [[1]]
+ * Constraints:
+ * 1 <= nums.length <= 6
+ * -10 <= nums[i] <= 10
+ * All the integers of nums are unique.
  */
 public class S046 {
 }
