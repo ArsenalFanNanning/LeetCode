37213f5068fd51f4b34bc1e11e7aea90b66a5915
Arsenal <funing.ma.china@gmail.com>
2022-12-30 23:54:03 +0800

    047 全排列 II

--- src/com/leetcode/one/hundred/S047.java
+++ src/com/leetcode/one/hundred/S047.java
@@ -1,5 +1,9 @@
 package com.leetcode.one.hundred;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @Author: Arsenal
  * @Date: 2022-12-27 23:56
@@ -33,4 +37,36 @@ package com.leetcode.one.hundred;
  * -10 <= nums[i] <= 10
  */
 public class S047 {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2};
+        System.out.println(permuteUnique(nums));
+    }
+
+    static boolean[] vis;
+
+    public static List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> perm = new ArrayList<Integer>();
+        vis = new boolean[nums.length];
+        Arrays.sort(nums);
+        backtrack(nums, ans, 0, perm);
+        return ans;
+    }
+
+    public static void backtrack(int[] nums, List<List<Integer>> ans, int idx, List<Integer> perm) {
+        if (idx == nums.length) {
+            ans.add(new ArrayList<Integer>(perm));
+            return;
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (vis[i] || (i > 0 && nums[i] == nums[i - 1] && !vis[i - 1])) {
+                continue;
+            }
+            perm.add(nums[i]);
+            vis[i] = true;
+            backtrack(nums, ans, idx + 1, perm);
+            vis[i] = false;
+            perm.remove(idx);
+        }
+    }
 }
