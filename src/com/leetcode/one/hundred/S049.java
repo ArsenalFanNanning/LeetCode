13e6ef8af66fad63db79f723d55f609625dabdc3
Arsenal <funing.ma.china@gmail.com>
2023-01-05 23:54:02 +0800

    049 字母异位词分组

--- src/com/leetcode/one/hundred/S049.java
+++ src/com/leetcode/one/hundred/S049.java
@@ -1,5 +1,7 @@
 package com.leetcode.one.hundred;
 
+import java.util.*;
+
 /**
  * @Author: Arsenal
  * @Date: 2023-01-03 23:48
@@ -37,4 +39,21 @@ package com.leetcode.one.hundred;
  * strs[i] consists of lowercase English letters.
  */
 public class S049 {
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
 }
