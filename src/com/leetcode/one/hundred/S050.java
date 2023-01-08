25b1914356ddee92e3f835e67cfd84405d8be152
Arsenal <funing.ma.china@gmail.com>
2023-01-09 00:04:18 +0800

    050 Pow(x, n)

--- src/com/leetcode/one/hundred/S050.java
+++ src/com/leetcode/one/hundred/S050.java
@@ -42,7 +42,8 @@ public class S050 {
     public static void main(String[] args) {
         double x = 2.0;
         int n = 10;
-        System.out.println(myPow(x, n));
+        //System.out.println(myPow(x, n));
+        System.out.println(myPowOpt(x, n));
     }
 
     public static double myPow(double x, int n) {
@@ -56,5 +57,29 @@ public class S050 {
         }
         double y = quickMul(x, N / 2);
         return N % 2 == 0 ? y * y : y * y * x;
+    }
+
+
+    public static double myPowOpt(double x, int n) {
+        long N = n;
+        return N >= 0 ? quickMulOpt(x, N) : 1.0 / quickMulOpt(x, -N);
+    }
+
+    public static double quickMulOpt(double x, long N) {
+        double ans = 1.0;
+        // 贡献的初始值为 x
+        double x_contribute = x;
+        // 在对 N 进行二进制拆分的同时计算答案
+        while (N > 0) {
+            if (N % 2 == 1) {
+                // 如果 N 二进制表示的最低位为 1，那么需要计入贡献
+                ans *= x_contribute;
+            }
+            // 将贡献不断地平方
+            x_contribute *= x_contribute;
+            // 舍弃 N 二进制表示的最低位，这样我们每次只要判断最低位即可
+            N /= 2;
+        }
+        return ans;
     }
 }
