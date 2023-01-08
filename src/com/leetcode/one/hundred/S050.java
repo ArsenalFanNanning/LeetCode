61d9dae255aff64f990739bb2a24ae5bbaeab93c
Arsenal <funing.ma.china@gmail.com>
2023-01-09 00:03:12 +0800

    050 Pow(x, n)

--- src/com/leetcode/one/hundred/S050.java
+++ src/com/leetcode/one/hundred/S050.java
@@ -39,4 +39,22 @@ package com.leetcode.one.hundred;
  * -104 <= xn <= 104
  */
 public class S050 {
+    public static void main(String[] args) {
+        double x = 2.0;
+        int n = 10;
+        System.out.println(myPow(x, n));
+    }
+
+    public static double myPow(double x, int n) {
+        long N = n;
+        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+    }
+
+    public static double quickMul(double x, long N) {
+        if (N == 0) {
+            return 1.0;
+        }
+        double y = quickMul(x, N / 2);
+        return N % 2 == 0 ? y * y : y * y * x;
+    }
 }
