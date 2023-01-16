7d0a6bbddadab97769f8ac84af0ffe319f2a3bf5
Arsenal <funing.ma.china@gmail.com>
2023-01-16 23:45:35 +0800

    052 N皇后 II

--- src/com/leetcode/one/hundred/S052.java
+++ src/com/leetcode/one/hundred/S052.java
@@ -1,5 +1,8 @@
 package com.leetcode.one.hundred;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @Author: Arsenal
  * @Date: 2023-01-14 23:29
@@ -29,4 +32,45 @@ package com.leetcode.one.hundred;
  * 1 <= n <= 9
  */
 public class S052 {
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(totalNQueens(n));
+    }
+
+    public static int totalNQueens(int n) {
+        Set<Integer> columns = new HashSet<Integer>();
+        Set<Integer> diagonals1 = new HashSet<Integer>();
+        Set<Integer> diagonals2 = new HashSet<Integer>();
+        return backtrack(n, 0, columns, diagonals1, diagonals2);
+    }
+
+    public static int backtrack(int n, int row, Set<Integer> columns, Set<Integer> diagonals1, Set<Integer> diagonals2) {
+        if (row == n) {
+            return 1;
+        } else {
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                if (columns.contains(i)) {
+                    continue;
+                }
+                int diagonal1 = row - i;
+                if (diagonals1.contains(diagonal1)) {
+                    continue;
+                }
+                int diagonal2 = row + i;
+                if (diagonals2.contains(diagonal2)) {
+                    continue;
+                }
+                columns.add(i);
+                diagonals1.add(diagonal1);
+                diagonals2.add(diagonal2);
+                count += backtrack(n, row + 1, columns, diagonals1, diagonals2);
+                columns.remove(i);
+                diagonals1.remove(diagonal1);
+                diagonals2.remove(diagonal2);
+            }
+            return count;
+        }
+    }
+
 }
