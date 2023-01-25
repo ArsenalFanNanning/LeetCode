bf4309cc1c8561e294b200d43b600006100f51db
Arsenal <funing.ma.china@gmail.com>
2023-01-25 16:44:42 +0800

    54 螺旋矩阵

--- src/com/leetcode/one/hundred/S054.java
+++ src/com/leetcode/one/hundred/S054.java
@@ -40,7 +40,8 @@ public class S054 {
                 {4, 5, 6},
                 {7, 8, 9}
         };
-        System.out.println(spiralOrder(matrix));
+        // System.out.println(spiralOrder(matrix));
+        System.out.println(spiralOrderOpt(matrix));
     }
 
     public static List<Integer> spiralOrder(int[][] matrix) {
@@ -63,6 +64,36 @@ public class S054 {
             }
             row += directions[directionIndex][0];
             column += directions[directionIndex][1];
+        }
+        return order;
+    }
+
+    public static List<Integer> spiralOrderOpt(int[][] matrix) {
+        List<Integer> order = new ArrayList<>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return order;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        int left = 0, right = columns - 1, top = 0, bottom = rows - 1;
+        while (left <= right && top <= bottom) {
+            for (int column = left; column <= right; column++) {
+                order.add(matrix[top][column]);
+            }
+            for (int row = top + 1; row <= bottom; row++) {
+                order.add(matrix[row][right]);
+            }
+            if (left < right && top < bottom) {
+                for (int column = right - 1; column > left; column--) {
+                    order.add(matrix[bottom][column]);
+                }
+                for (int row = bottom; row > top; row--) {
+                    order.add(matrix[row][left]);
+                }
+            }
+            left++;
+            right--;
+            top++;
+            bottom--;
         }
         return order;
     }
