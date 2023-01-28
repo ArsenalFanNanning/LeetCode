5ac1a35e68e1c16a511abec4ceb52c2cf0fb6eb6
Arsenal <funing.ma.china@gmail.com>
2023-01-28 21:10:36 +0800

    55 跳跃游戏

--- src/com/leetcode/one/hundred/S055.java
+++ src/com/leetcode/one/hundred/S055.java
@@ -35,4 +35,22 @@ package com.leetcode.one.hundred;
  * 0 <= nums[i] <= 105
  */
 public class S055 {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 1, 4};
+        System.out.println(canJump(nums));
+    }
+
+    public static boolean canJump(int[] nums) {
+        int n = nums.length;
+        int rightmost = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i <= rightmost) {
+                rightmost = Math.max(rightmost, i + nums[i]);
+                if (rightmost >= n - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
