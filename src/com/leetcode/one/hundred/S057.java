92d0299eb432dbde82396df43fb0da5af1f2ba58
Arsenal <funing.ma.china@gmail.com>
2023-02-03 23:51:24 +0800

    056 插入区间

--- src/com/leetcode/one/hundred/S057.java
+++ src/com/leetcode/one/hundred/S057.java
@@ -1,5 +1,9 @@
 package com.leetcode.one.hundred;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @Author: Arsenal
  * @Date: 2023-02-01 23:42
@@ -53,6 +57,43 @@ package com.leetcode.one.hundred;
  */
 public class S057 {
     public static void main(String[] args) {
+        int[][] intervals = {{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}};
+        int[] newInterval = {2, 5};
+        int[][] insert = insert(intervals, newInterval);
+        for (int[] ints : insert) {
+            System.out.println(Arrays.toString(ints));
+        }
+    }
 
+    public static int[][] insert(int[][] intervals, int[] newInterval) {
+        int left = newInterval[0];
+        int right = newInterval[1];
+        boolean placed = false;
+        List<int[]> ansList = new ArrayList<int[]>();
+        for (int[] interval : intervals) {
+            if (interval[0] > right) {
+                // 在插入区间的右侧且无交集
+                if (!placed) {
+                    ansList.add(new int[]{left, right});
+                    placed = true;
+                }
+                ansList.add(interval);
+            } else if (interval[1] < left) {
+                // 在插入区间的左侧且无交集
+                ansList.add(interval);
+            } else {
+                // 与插入区间有交集，计算它们的并集
+                left = Math.min(left, interval[0]);
+                right = Math.max(right, interval[1]);
+            }
+        }
+        if (!placed) {
+            ansList.add(new int[]{left, right});
+        }
+        int[][] ans = new int[ansList.size()][2];
+        for (int i = 0; i < ansList.size(); ++i) {
+            ans[i] = ansList.get(i);
+        }
+        return ans;
     }
 }
