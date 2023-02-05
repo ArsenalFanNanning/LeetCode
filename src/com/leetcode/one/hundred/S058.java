ce04a06cfdfe50e9562b814c146a0c94f65ceb2c
Arsenal <funing.ma.china@gmail.com>
2023-02-05 23:52:42 +0800

    058 最后一个单词的长度

--- src/com/leetcode/one/hundred/S058.java
+++ src/com/leetcode/one/hundred/S058.java
@@ -22,6 +22,27 @@ package com.leetcode.one.hundred;
  * 1 <= s.length <= 104
  * s 仅有英文字母和空格 ' ' 组成
  * s 中至少存在一个单词
+ * 58. Length of Last Word
+ * Given a string s consisting of words and spaces, return the length of the last word in the string.
+ * A word is a maximal
+ * substring
+ * consisting of non-space characters only.
+ * Example 1:
+ * Input: s = "Hello World"
+ * Output: 5
+ * Explanation: The last word is "World" with length 5.
+ * Example 2:
+ * Input: s = "   fly me   to   the moon  "
+ * Output: 4
+ * Explanation: The last word is "moon" with length 4.
+ * Example 3:
+ * Input: s = "luffy is still joyboy"
+ * Output: 6
+ * Explanation: The last word is "joyboy" with length 6.
+ * Constraints:
+ * 1 <= s.length <= 104
+ * s consists of only English letters and spaces ' '.
+ * There will be at least one word in s.
  */
 public class S058 {
 }
