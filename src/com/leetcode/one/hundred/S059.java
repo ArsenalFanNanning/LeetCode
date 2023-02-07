db54462b415bc2415b380e802c148648ab022f6e
Arsenal <funing.ma.china@gmail.com>
2023-02-07 23:23:17 +0800

    059 螺旋矩阵 II

---
+++ src/com/leetcode/one/hundred/S059.java
@@ -0,0 +1,18 @@
+package com.leetcode.one.hundred;
+
+/**
+ * @Author: Arsenal
+ * @Date: 2023-02-07 23:22
+ * @Description: 螺旋矩阵 II -中等
+ * 给你一个正整数 n ，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。
+ * 示例 1：
+ * 输入：n = 3
+ * 输出：[[1,2,3],[8,9,4],[7,6,5]]
+ * 示例 2：
+ * 输入：n = 1
+ * 输出：[[1]]
+ * 提示：
+ * 1 <= n <= 20
+ */
+public class S059 {
+}
