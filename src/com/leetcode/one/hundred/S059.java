d8d2b906bce5ff577479bea5519012b3457247db
Arsenal <funing.ma.china@gmail.com>
2023-02-08 23:19:47 +0800

    059 螺旋矩阵 II

--- src/com/leetcode/one/hundred/S059.java
+++ src/com/leetcode/one/hundred/S059.java
@@ -1,5 +1,7 @@
 package com.leetcode.one.hundred;
 
+import java.util.Arrays;
+
 /**
  * @Author: Arsenal
  * @Date: 2023-02-07 23:22
@@ -25,4 +27,32 @@ package com.leetcode.one.hundred;
  * 1 <= n <= 20
  */
 public class S059 {
+    public static void main(String[] args) {
+        int n = 3;
+        int[][] matrix = generateMatrix(n);
+        for (int[] ints : matrix) {
+            System.out.println(Arrays.toString(ints));
+        }
+    }
+
+    public static int[][] generateMatrix(int n) {
+        int maxNum = n * n;
+        int curNum = 1;
+        int[][] matrix = new int[n][n];
+        int row = 0, column = 0;
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // 右下左上
+        int directionIndex = 0;
+        while (curNum <= maxNum) {
+            matrix[row][column] = curNum;
+            curNum++;
+            int nextRow = row + directions[directionIndex][0], nextColumn = column + directions[directionIndex][1];
+            if (nextRow < 0 || nextRow >= n || nextColumn < 0 || nextColumn >= n || matrix[nextRow][nextColumn] != 0) {
+                directionIndex = (directionIndex + 1) % 4; // 顺时针旋转至下一个方向
+            }
+            row = row + directions[directionIndex][0];
+            column = column + directions[directionIndex][1];
+        }
+        return matrix;
+    }
+
 }
