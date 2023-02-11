0e78dced03596e82f6395972f3f8003430501f50
Arsenal <funing.ma.china@gmail.com>
2023-02-12 00:02:28 +0800

    060 排列序列

--- src/com/leetcode/one/hundred/S060.java
+++ src/com/leetcode/one/hundred/S060.java
@@ -1,5 +1,7 @@
 package com.leetcode.one.hundred;
 
+import java.util.Arrays;
+
 /**
  * @Author: Arsenal
  * @Date: 2023-02-10 23:52
@@ -49,4 +51,34 @@ package com.leetcode.one.hundred;
  * 1 <= k <= n!
  */
 public class S060 {
+    public static void main(String[] args) {
+        int n = 3, k = 3;
+        System.out.println(getPermutation(n, k));
+    }
+
+    public static String getPermutation(int n, int k) {
+        int[] factorial = new int[n];
+        factorial[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            factorial[i] = factorial[i - 1] * i;
+        }
+
+        --k;
+        StringBuffer ans = new StringBuffer();
+        int[] valid = new int[n + 1];
+        Arrays.fill(valid, 1);
+        for (int i = 1; i <= n; ++i) {
+            int order = k / factorial[n - i] + 1;
+            for (int j = 1; j <= n; ++j) {
+                order -= valid[j];
+                if (order == 0) {
+                    ans.append(j);
+                    valid[j] = 0;
+                    break;
+                }
+            }
+            k %= factorial[n - i];
+        }
+        return ans.toString();
+    }
 }
