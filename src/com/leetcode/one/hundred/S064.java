5b6c405c8a5beff811cccc3004672b79ce6656dc
Arsenal <funing.ma.china@gmail.com>
2023-02-25 15:26:25 +0800

    064 最小路径和

--- src/com/leetcode/one/hundred/S064.java
+++ src/com/leetcode/one/hundred/S064.java
@@ -36,4 +36,33 @@ package com.leetcode.one.hundred;
  * 0 <= grid[i][j] <= 100
  */
 public class S064 {
+    public static void main(String[] args) {
+        int[][] grid = {
+                {1, 3, 1},
+                {1, 5, 1},
+                {4, 2, 1}
+        };
+        System.out.println(minPathSum(grid));
+    }
+
+    public static int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int rows = grid.length, columns = grid[0].length;
+        int[][] dp = new int[rows][columns];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < rows; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < columns; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < columns; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[rows - 1][columns - 1];
+    }
 }
