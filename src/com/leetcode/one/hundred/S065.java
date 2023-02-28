9644584c978659be3f95bf810df91f24342fea56
Arsenal <funing.ma.china@gmail.com>
2023-03-01 00:11:16 +0800

    065 有效数字

--- src/com/leetcode/one/hundred/S065.java
+++ src/com/leetcode/one/hundred/S065.java
@@ -1,76 +1,36 @@
 package com.leetcode.one.hundred;
 
-import java.util.Arrays;
-
 /**
  * @Author: Arsenal
- * @Date: 2023-02-26 23:49
- * @Description: 加一 -简单
- * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
- * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
- * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ * @Date: 2023-03-01 00:07
+ * @Description: 有效数字 -困难
+ * 有效数字（按顺序）可以分成以下几个部分：
+ * 一个 小数 或者 整数
+ * （可选）一个 'e' 或 'E' ，后面跟着一个 整数
+ * 小数（按顺序）可以分成以下几个部分：
+ * （可选）一个符号字符（'+' 或 '-'）
+ * 下述格式之一：
+ * 至少一位数字，后面跟着一个点 '.'
+ * 至少一位数字，后面跟着一个点 '.' ，后面再跟着至少一位数字
+ * 一个点 '.' ，后面跟着至少一位数字
+ * 整数（按顺序）可以分成以下几个部分：
+ * （可选）一个符号字符（'+' 或 '-'）
+ * 至少一位数字
+ * 部分有效数字列举如下：["2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789"]
+ * 部分无效数字列举如下：["abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53"]
+ * 给你一个字符串 s ，如果 s 是一个 有效数字 ，请返回 true 。
  * 示例 1：
- * 输入：digits = [1,2,3]
- * 输出：[1,2,4]
- * 解释：输入数组表示数字 123。
+ * 输入：s = "0"
+ * 输出：true
  * 示例 2：
- * 输入：digits = [4,3,2,1]
- * 输出：[4,3,2,2]
- * 解释：输入数组表示数字 4321。
+ * 输入：s = "e"
+ * 输出：false
  * 示例 3：
- * 输入：digits = [0]
- * 输出：[1]
+ * 输入：s = "."
+ * 输出：false
  * 提示：
- * 1 <= digits.length <= 100
- * 0 <= digits[i] <= 9
- * 66. Plus One
- * You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
- * The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
- * Increment the large integer by one and return the resulting array of digits.
- * Example 1:
- * Input: digits = [1,2,3]
- * Output: [1,2,4]
- * Explanation: The array represents the integer 123.
- * Incrementing by one gives 123 + 1 = 124.
- * Thus, the result should be [1,2,4].
- * Example 2:
- * Input: digits = [4,3,2,1]
- * Output: [4,3,2,2]
- * Explanation: The array represents the integer 4321.
- * Incrementing by one gives 4321 + 1 = 4322.
- * Thus, the result should be [4,3,2,2].
- * Example 3:
- * Input: digits = [9]
- * Output: [1,0]
- * Explanation: The array represents the integer 9.
- * Incrementing by one gives 9 + 1 = 10.
- * Thus, the result should be [1,0].
- * Constraints:
- * 1 <= digits.length <= 100
- * 0 <= digits[i] <= 9
- * digits does not contain any leading 0's.
+ * 1 <= s.length <= 20
+ * s 仅含英文字母（大写和小写），数字（0-9），加号 '+' ，减号 '-' ，或者点 '.' 。
  */
 public class S065 {
-    public static void main(String[] args) {
-        int[] digits = {1, 2, 3};
-        System.out.println(Arrays.toString(plusOne(digits)));
-    }
-
-    public static int[] plusOne(int[] digits) {
-        int n = digits.length;
-        for (int i = n - 1; i >= 0; --i) {
-            if (digits[i] != 9) {
-                ++digits[i];
-                for (int j = i + 1; j < n; ++j) {
-                    digits[j] = 0;
-                }
-                return digits;
-            }
-        }
-
-        // digits 中所有的元素均为 9
-        int[] ans = new int[n + 1];
-        ans[0] = 1;
-        return ans;
-    }
 }
