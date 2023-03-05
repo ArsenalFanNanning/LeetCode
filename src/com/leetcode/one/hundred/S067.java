06ded0c74a053437e027be1dfc2e6fb6fbd08b5b
Arsenal <funing.ma.china@gmail.com>
2023-03-06 00:03:38 +0800

    067 二进制求和

--- src/com/leetcode/one/hundred/S067.java
+++ src/com/leetcode/one/hundred/S067.java
@@ -15,6 +15,18 @@ package com.leetcode.one.hundred;
  * 1 <= a.length, b.length <= 104
  * a 和 b 仅由字符 '0' 或 '1' 组成
  * 字符串如果不是 "0" ，就不含前导零
+ * 67. Add Binary
+ * Given two binary strings a and b, return their sum as a binary string.
+ * Example 1:
+ * Input: a = "11", b = "1"
+ * Output: "100"
+ * Example 2:
+ * Input: a = "1010", b = "1011"
+ * Output: "10101"
+ * Constraints:
+ * 1 <= a.length, b.length <= 104
+ * a and b consist only of '0' or '1' characters.
+ * Each string does not contain leading zeros except for the zero itself.
  */
 public class S067 {
 }
