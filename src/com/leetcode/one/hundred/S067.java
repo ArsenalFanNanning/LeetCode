f18424b0676d17898ca7ccfb0e8f5d7b43640cb8
Arsenal <funing.ma.china@gmail.com>
2023-03-08 23:32:05 +0800

    067 二进制求和

--- src/com/leetcode/one/hundred/S067.java
+++ src/com/leetcode/one/hundred/S067.java
@@ -32,6 +32,7 @@ public class S067 {
     public static void main(String[] args) {
         String a = "11", b = "1";
         System.out.println(addBinary(a, b));
+        System.out.println(addBinaryOpt(a, b));
     }
 
     public static String addBinary(String a, String b) {
@@ -39,4 +40,24 @@ public class S067 {
                 Integer.parseInt(a, 2) + Integer.parseInt(b, 2)
         );
     }
+
+    public static String addBinaryOpt(String a, String b) {
+        StringBuffer ans = new StringBuffer();
+
+        int n = Math.max(a.length(), b.length()), carry = 0;
+        for (int i = 0; i < n; ++i) {
+            carry += i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0;
+            carry += i < b.length() ? (b.charAt(b.length() - 1 - i) - '0') : 0;
+            ans.append((char) (carry % 2 + '0'));
+            carry /= 2;
+        }
+
+        if (carry > 0) {
+            ans.append('1');
+        }
+        ans.reverse();
+
+        return ans.toString();
+    }
+
 }
