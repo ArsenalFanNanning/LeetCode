0b7d1bf71bfa6db87aa61dc2640847e9de7d532a
Arsenal <funing.ma.china@gmail.com>
2023-03-13 00:08:59 +0800

    069 x 的平方根

--- src/com/leetcode/one/hundred/S069.java
+++ src/com/leetcode/one/hundred/S069.java
@@ -15,7 +15,23 @@ package com.leetcode.one.hundred;
  * 输出：2
  * 解释：8 的算术平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
  * 提示：
- *     0 <= x <= 231 - 1
+ * 0 <= x <= 231 - 1
+ * 69. Sqrt(x)
+ * Given a non-negative integer x, return the square root of x rounded down to the nearest integer.
+ * The returned integer should be non-negative as well.
+ * You must not use any built-in exponent function or operator.
+ * For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ * Example 1:
+ * Input: x = 4
+ * Output: 2
+ * Explanation: The square root of 4 is 2, so we return 2.
+ * Example 2:
+ * Input: x = 8
+ * Output: 2
+ * Explanation: The square root of 8 is 2.82842...,
+ * and since we round it down to the nearest integer, 2 is returned.
+ * Constraints:
+ * 0 <= x <= 231 - 1
  */
 public class S069 {
 }
