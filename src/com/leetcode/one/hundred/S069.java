de409f8cc38138dc82aa89dbf5c2909a051ff9d7
Arsenal <funing.ma.china@gmail.com>
2023-03-14 23:34:19 +0800

    069 x 的平方根

--- src/com/leetcode/one/hundred/S069.java
+++ src/com/leetcode/one/hundred/S069.java
@@ -34,4 +34,17 @@ package com.leetcode.one.hundred;
  * 0 <= x <= 231 - 1
  */
 public class S069 {
+    public static void main(String[] args) {
+        int x = 4;
+        System.out.println(mySqrt(x));
+    }
+
+    public static int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        int ans = (int) Math.exp(0.5 * Math.log(x));
+        return (long) (ans + 1) * (ans + 1) <= x ? ans + 1 : ans;
+    }
+
 }
