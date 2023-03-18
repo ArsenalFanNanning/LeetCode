9f8e6b53727d471dd83ac89350c446da380c330a
Arsenal <funing.ma.china@gmail.com>
2023-03-18 16:19:04 +0800

    070 爬楼梯

--- src/com/leetcode/one/hundred/S070.java
+++ src/com/leetcode/one/hundred/S070.java
@@ -41,4 +41,19 @@ package com.leetcode.one.hundred;
  * 1 <= n <= 45
  */
 public class S070 {
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(climbStairs(n));
+    }
+
+
+    public static int climbStairs(int n) {
+        int p = 0, q = 0, r = 1;
+        for (int i = 1; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
+    }
 }
