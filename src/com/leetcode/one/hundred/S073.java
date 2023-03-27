f1e1c6da9325f9816e85df310ca1e267590096b3
Arsenal <funing.ma.china@gmail.com>
2023-03-28 00:01:28 +0800

    073 矩阵置零

--- src/com/leetcode/one/hundred/S073.java
+++ src/com/leetcode/one/hundred/S073.java
@@ -20,6 +20,24 @@ package com.leetcode.one.hundred;
  * 一个直观的解决方案是使用  O(mn) 的额外空间，但这并不是一个好的解决方案。
  * 一个简单的改进方案是使用 O(m + n) 的额外空间，但这仍然不是最好的解决方案。
  * 你能想出一个仅使用常量空间的解决方案吗？
+ * 73. Set Matrix Zeroes
+ * Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+ * You must do it in place.
+ * Example 1:
+ * Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+ * Output: [[1,0,1],[0,0,0],[1,0,1]]
+ * Example 2:
+ * Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+ * Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ * Constraints:
+ * m == matrix.length
+ * n == matrix[0].length
+ * 1 <= m, n <= 200
+ * -231 <= matrix[i][j] <= 231 - 1
+ * Follow up:
+ * A straightforward solution using O(mn) space is probably a bad idea.
+ * A simple improvement uses O(m + n) space, but still not the best solution.
+ * Could you devise a constant space solution?
  */
 public class S073 {
 }
