37f01cfbeb755728cbfe12cc4f9c40923b8e8cff
Arsenal <funing.ma.china@gmail.com>
2023-03-30 00:03:32 +0800

    073 矩阵置零

--- src/com/leetcode/one/hundred/S073.java
+++ src/com/leetcode/one/hundred/S073.java
@@ -49,7 +49,8 @@ public class S073 {
                 {1, 1, 1}
         };
 
-        setZeroes(matrix);
+        //setZeroes(matrix);
+        setZeroes1(matrix);
 
         for (int[] ints : matrix) {
             System.out.println(Arrays.toString(ints));
@@ -75,5 +76,45 @@ public class S073 {
             }
         }
     }
+
+    public static void setZeroes1(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean flagCol0 = false, flagRow0 = false;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                flagCol0 = true;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                flagRow0 = true;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (flagCol0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if (flagRow0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
 
 }
