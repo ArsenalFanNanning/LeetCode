282d435b2cbaa58b834d7b1dd32107311143f775
Arsenal <funing.ma.china@gmail.com>
2023-04-04 23:13:31 +0800

    074 搜索二维矩阵

--- src/com/leetcode/one/hundred/S074.java
+++ src/com/leetcode/one/hundred/S074.java
@@ -44,7 +44,9 @@ public class S074 {
                 {23, 30, 34, 60}
         };
         int target = 3;
-        System.out.println(searchMatrix(matrix, target));
+
+        // System.out.println(searchMatrix(matrix, target));
+        System.out.println(searchMatrixOpt(matrix, target));
     }
 
     public static boolean searchMatrix(int[][] matrix, int target) {
@@ -78,6 +80,23 @@ public class S074 {
                 high = mid - 1;
             } else {
                 low = mid + 1;
+            }
+        }
+        return false;
+    }
+
+    public static boolean searchMatrixOpt(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int low = 0, high = m * n - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int x = matrix[mid / n][mid % n];
+            if (x < target) {
+                low = mid + 1;
+            } else if (x > target) {
+                high = mid - 1;
+            } else {
+                return true;
             }
         }
         return false;
