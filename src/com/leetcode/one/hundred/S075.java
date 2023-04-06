058822d33d2a25e5a11ea5d7127773c7867c3ccb
Arsenal <funing.ma.china@gmail.com>
2023-04-06 23:40:18 +0800

    075 颜色分类

--- src/com/leetcode/one/hundred/S075.java
+++ src/com/leetcode/one/hundred/S075.java
@@ -19,6 +19,22 @@ package com.leetcode.one.hundred;
  * nums[i] 为 0、1 或 2
  * 进阶：
  * 你能想出一个仅使用常数空间的一趟扫描算法吗？
+ * 75. Sort Colors
+ * Given an array nums with n objects colored red, white, or blue, sort them in-place
+ * so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+ * We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+ * You must solve this problem without using the library's sort function.
+ * Example 1:
+ * Input: nums = [2,0,2,1,1,0]
+ * Output: [0,0,1,1,2,2]
+ * Example 2:
+ * Input: nums = [2,0,1]
+ * Output: [0,1,2]
+ * Constraints:
+ * n == nums.length
+ * 1 <= n <= 300
+ * nums[i] is either 0, 1, or 2.
+ * Follow up: Could you come up with a one-pass algorithm using only constant extra space?
  */
 public class S075 {
 }
