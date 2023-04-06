d6a216259a8abd0d40530f4315227cf69724a201
Arsenal <funing.ma.china@gmail.com>
2023-04-07 00:02:53 +0800

    075 颜色分类

--- src/com/leetcode/one/hundred/S075.java
+++ src/com/leetcode/one/hundred/S075.java
@@ -41,7 +41,8 @@ import java.util.Arrays;
 public class S075 {
     public static void main(String[] args) {
         int[] nums = {2, 0, 2, 1, 1, 0};
-        sortColors1(nums);
+        // sortColors1(nums);
+        sortColors2(nums);
         System.out.println(Arrays.toString(nums));
     }
 
@@ -65,5 +66,30 @@ public class S075 {
             }
         }
     }
+
+    public static void sortColors2(int[] nums) {
+        int n = nums.length;
+        int p0 = 0, p1 = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 1) {
+                int temp = nums[i];
+                nums[i] = nums[p1];
+                nums[p1] = temp;
+                ++p1;
+            } else if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[p0];
+                nums[p0] = temp;
+                if (p0 < p1) {
+                    temp = nums[i];
+                    nums[i] = nums[p1];
+                    nums[p1] = temp;
+                }
+                ++p0;
+                ++p1;
+            }
+        }
+    }
+
 
 }
