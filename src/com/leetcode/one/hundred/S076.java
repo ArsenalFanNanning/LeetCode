52a499c22b1e0094a3f310f4a565dd5e0a258db5
Arsenal <funing.ma.china@gmail.com>
2023-04-11 00:02:32 +0800

    076 最小覆盖子串

--- src/com/leetcode/one/hundred/S076.java
+++ src/com/leetcode/one/hundred/S076.java
@@ -1,5 +1,9 @@
 package com.leetcode.one.hundred;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  * @Author: Arsenal
  * @Date: 2023-04-09 16:32
@@ -54,4 +58,54 @@ package com.leetcode.one.hundred;
  * Follow up: Could you find an algorithm that runs in O(m + n) time?
  */
 public class S076 {
+    public static void main(String[] args) {
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+        System.out.println(minWindow(s, t));
+    }
+
+    static Map<Character, Integer> ori = new HashMap<Character, Integer>();
+    static Map<Character, Integer> cnt = new HashMap<Character, Integer>();
+
+    public static String minWindow(String s, String t) {
+        int tLen = t.length();
+        for (int i = 0; i < tLen; i++) {
+            char c = t.charAt(i);
+            ori.put(c, ori.getOrDefault(c, 0) + 1);
+        }
+        int l = 0, r = -1;
+        int len = Integer.MAX_VALUE, ansL = -1, ansR = -1;
+        int sLen = s.length();
+        while (r < sLen) {
+            ++r;
+            if (r < sLen && ori.containsKey(s.charAt(r))) {
+                cnt.put(s.charAt(r), cnt.getOrDefault(s.charAt(r), 0) + 1);
+            }
+            while (check() && l <= r) {
+                if (r - l + 1 < len) {
+                    len = r - l + 1;
+                    ansL = l;
+                    ansR = l + len;
+                }
+                if (ori.containsKey(s.charAt(l))) {
+                    cnt.put(s.charAt(l), cnt.getOrDefault(s.charAt(l), 0) - 1);
+                }
+                ++l;
+            }
+        }
+        return ansL == -1 ? "" : s.substring(ansL, ansR);
+    }
+
+    public static boolean check() {
+        Iterator iter = ori.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry entry = (Map.Entry) iter.next();
+            Character key = (Character) entry.getKey();
+            Integer val = (Integer) entry.getValue();
+            if (cnt.getOrDefault(key, 0) < val) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
