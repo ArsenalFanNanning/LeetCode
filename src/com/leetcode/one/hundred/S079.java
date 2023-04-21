75be2ab39d923a0a68cee8808b61970f6283ff98
Arsenal <funing.ma.china@gmail.com>
2023-04-21 21:01:28 +0800

    079 单词搜索

--- src/com/leetcode/one/hundred/S079.java
+++ src/com/leetcode/one/hundred/S079.java
@@ -22,6 +22,26 @@ package com.leetcode.one.hundred;
  * 1 <= word.length <= 15
  * board 和 word 仅由大小写英文字母组成
  * 进阶：你可以使用搜索剪枝的技术来优化解决方案，使其在 board 更大的情况下可以更快解决问题？
+ * 79. Word Search
+ * Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+ * The word can be constructed from letters of sequentially adjacent cells,
+ * where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+ * Example 1:
+ * Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+ * Output: true
+ * Example 2:
+ * Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+ * Output: true
+ * Example 3:
+ * Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+ * Output: false
+ * Constraints:
+ * m == board.length
+ * n = board[i].length
+ * 1 <= m, n <= 6
+ * 1 <= word.length <= 15
+ * board and word consists of only lowercase and uppercase English letters.
+ * Follow up: Could you use search pruning to make your solution faster with a larger board?
  */
 public class S079 {
 }
