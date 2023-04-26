0b3bc8f92b0ee4d6996e33b47b21058ca24f7e9d
Arsenal <funing.ma.china@gmail.com>
2023-04-26 23:12:51 +0800

    080 删除有序数组中的重复项 II

--- src/com/leetcode/one/hundred/S080.java
+++ src/com/leetcode/one/hundred/S080.java
@@ -64,4 +64,25 @@ package com.leetcode.one.hundred;
  * nums is sorted in non-decreasing order.
  */
 public class S080 {
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 1, 2, 3, 3};
+        System.out.println(removeDuplicates(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n <= 2) {
+            return n;
+        }
+        int slow = 2, fast = 2;
+        while (fast < n) {
+            if (nums[slow - 2] != nums[fast]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
+
 }
