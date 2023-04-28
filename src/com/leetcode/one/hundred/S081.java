af14ed4e895aec019ff55270e1aaf461e4cbdd81
Arsenal <funing.ma.china@gmail.com>
2023-04-29 07:24:55 +0800

    081 搜索旋转排序数组 II

--- src/com/leetcode/one/hundred/S081.java
+++ src/com/leetcode/one/hundred/S081.java
@@ -47,4 +47,43 @@ package com.leetcode.one.hundred;
  * but nums may contain duplicates. Would this affect the runtime complexity? How and why?
  */
 public class S081 {
+    public static void main(String[] args) {
+        int[] nums = {2, 5, 6, 0, 0, 1, 2};
+        int target = 0;
+        System.out.println(search(nums, target));
+    }
+
+    public static boolean search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return false;
+        }
+        if (n == 1) {
+            return nums[0] == target;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[l] == nums[mid] && nums[mid] == nums[r]) {
+                ++l;
+                --r;
+            } else if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
 }
