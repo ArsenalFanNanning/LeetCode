72f5e1125f3192768fdc93ef679edb0a13c54c67
Arsenal <funing.ma.china@gmail.com>
2023-05-04 23:14:52 +0800

    083 删除排序链表中的重复元素

--- src/com/leetcode/one/hundred/S083.java
+++ src/com/leetcode/one/hundred/S083.java
@@ -16,6 +16,19 @@ package com.leetcode.one.hundred;
  * 链表中节点数目在范围 [0, 300] 内
  * -100 <= Node.val <= 100
  * 题目数据保证链表已经按升序 排列
+ * 83. Remove Duplicates from Sorted List
+ * Given the head of a sorted linked list, delete all duplicates such
+ * that each element appears only once. Return the linked list sorted as well.
+ * Example 1:
+ * Input: head = [1,1,2]
+ * Output: [1,2]
+ * Example 2:
+ * Input: head = [1,1,2,3,3]
+ * Output: [1,2,3]
+ * Constraints:
+ * The number of nodes in the list is in the range [0, 300].
+ * -100 <= Node.val <= 100
+ * The list is guaranteed to be sorted in ascending order.
  */
 public class S083 {
 }
