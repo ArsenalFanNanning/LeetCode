e020e8a510c7dc68bd4e3a708c50de42b1d4c4b6
Arsenal <funing.ma.china@gmail.com>
2023-05-08 00:04:04 +0800

    084 柱状图中最大的矩形

--- src/com/leetcode/one/hundred/S084.java
+++ src/com/leetcode/one/hundred/S084.java
@@ -1,5 +1,8 @@
 package com.leetcode.one.hundred;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * @Author: Arsenal
  * @Date: 2023-05-06 23:07
@@ -32,4 +35,38 @@ package com.leetcode.one.hundred;
  * 0 <= heights[i] <= 104
  */
 public class S084 {
+    public static void main(String[] args) {
+        int[] heights = {2, 1, 5, 6, 2, 3};
+        System.out.println(largestRectangleArea(heights));
+    }
+
+    public static int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        Deque<Integer> mono_stack = new ArrayDeque<Integer>();
+        for (int i = 0; i < n; ++i) {
+            while (!mono_stack.isEmpty() && heights[mono_stack.peek()] >= heights[i]) {
+                mono_stack.pop();
+            }
+            left[i] = (mono_stack.isEmpty() ? -1 : mono_stack.peek());
+            mono_stack.push(i);
+        }
+
+        mono_stack.clear();
+        for (int i = n - 1; i >= 0; --i) {
+            while (!mono_stack.isEmpty() && heights[mono_stack.peek()] >= heights[i]) {
+                mono_stack.pop();
+            }
+            right[i] = (mono_stack.isEmpty() ? n : mono_stack.peek());
+            mono_stack.push(i);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
+    }
 }
