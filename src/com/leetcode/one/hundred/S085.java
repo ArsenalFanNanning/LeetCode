15af08fc52b3c1e61cdac48ec715a906410a5fa9
Arsenal <funing.ma.china@gmail.com>
2023-05-13 17:32:39 +0800

    085 最大矩形

--- src/com/leetcode/one/hundred/S085.java
+++ src/com/leetcode/one/hundred/S085.java
@@ -1,5 +1,8 @@
 package com.leetcode.one.hundred;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @Author: Arsenal
  * @Date: 2023-05-10 00:01
@@ -53,7 +56,9 @@ public class S085 {
                 {'1', '1', '1', '1', '1'},
                 {'1', '0', '0', '1', '0'}
         };
-        System.out.println(maximalRectangle(matrix));
+
+        // System.out.println(maximalRectangle(matrix));
+        System.out.println(maximalRectangleOpt(matrix));
     }
 
     public static int maximalRectangle(char[][] matrix) {
@@ -84,6 +89,54 @@ public class S085 {
                     width = Math.min(width, left[k][j]);
                     area = Math.max(area, (i - k + 1) * width);
                 }
+                ret = Math.max(ret, area);
+            }
+        }
+        return ret;
+    }
+
+
+    public static int maximalRectangleOpt(char[][] matrix) {
+        int m = matrix.length;
+        if (m == 0) {
+            return 0;
+        }
+        int n = matrix[0].length;
+        int[][] left = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    left[i][j] = (j == 0 ? 0 : left[i][j - 1]) + 1;
+                }
+            }
+        }
+
+        int ret = 0;
+        for (int j = 0; j < n; j++) { // 对于每一列，使用基于柱状图的方法
+            int[] up = new int[m];
+            int[] down = new int[m];
+
+            Deque<Integer> stack = new LinkedList<Integer>();
+            for (int i = 0; i < m; i++) {
+                while (!stack.isEmpty() && left[stack.peek()][j] >= left[i][j]) {
+                    stack.pop();
+                }
+                up[i] = stack.isEmpty() ? -1 : stack.peek();
+                stack.push(i);
+            }
+            stack.clear();
+            for (int i = m - 1; i >= 0; i--) {
+                while (!stack.isEmpty() && left[stack.peek()][j] >= left[i][j]) {
+                    stack.pop();
+                }
+                down[i] = stack.isEmpty() ? m : stack.peek();
+                stack.push(i);
+            }
+
+            for (int i = 0; i < m; i++) {
+                int height = down[i] - up[i] - 1;
+                int area = height * left[i][j];
                 ret = Math.max(ret, area);
             }
         }
