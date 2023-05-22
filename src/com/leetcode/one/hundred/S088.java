d9b03a30244032f2d32daf60be3b5fc9e0587c27
Arsenal <funing.ma.china@gmail.com>
2023-05-23 00:02:59 +0800

    088 合并两个有序数组

--- src/com/leetcode/one/hundred/S088.java
+++ src/com/leetcode/one/hundred/S088.java
@@ -70,7 +70,8 @@ public class S088 {
         int m = 3;
         int[] nums2 = {2, 5, 6};
         int n = 3;
-        merge(nums1, m, nums2, n);
+        //merge(nums1, m, nums2, n);
+        mergeOpt(nums1, m, nums2, n);
         System.out.println(Arrays.toString(nums1));
     }
 
@@ -79,5 +80,26 @@ public class S088 {
             nums1[m + i] = nums2[i];
         }
         Arrays.sort(nums1);
+    }
+
+    public static void mergeOpt(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = 0, p2 = 0;
+        int[] sorted = new int[m + n];
+        int cur;
+        while (p1 < m || p2 < n) {
+            if (p1 == m) {
+                cur = nums2[p2++];
+            } else if (p2 == n) {
+                cur = nums1[p1++];
+            } else if (nums1[p1] < nums2[p2]) {
+                cur = nums1[p1++];
+            } else {
+                cur = nums2[p2++];
+            }
+            sorted[p1 + p2 - 1] = cur;
+        }
+        for (int i = 0; i != m + n; ++i) {
+            nums1[i] = sorted[i];
+        }
     }
 }
