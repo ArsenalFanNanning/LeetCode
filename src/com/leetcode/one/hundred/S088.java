e2a9fb258dd136eae860c9f7d40b221b09ab0c02
Arsenal <funing.ma.china@gmail.com>
2023-05-24 23:48:22 +0800

    088 合并两个有序数组

--- src/com/leetcode/one/hundred/S088.java
+++ src/com/leetcode/one/hundred/S088.java
@@ -70,19 +70,20 @@ public class S088 {
         int m = 3;
         int[] nums2 = {2, 5, 6};
         int n = 3;
-        //merge(nums1, m, nums2, n);
-        mergeOpt(nums1, m, nums2, n);
+        //merge1(nums1, m, nums2, n);
+        //merge2(nums1, m, nums2, n);
+        merge3(nums1, m, nums2, n);
         System.out.println(Arrays.toString(nums1));
     }
 
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+    public static void merge1(int[] nums1, int m, int[] nums2, int n) {
         for (int i = 0; i != n; ++i) {
             nums1[m + i] = nums2[i];
         }
         Arrays.sort(nums1);
     }
 
-    public static void mergeOpt(int[] nums1, int m, int[] nums2, int n) {
+    public static void merge2(int[] nums1, int m, int[] nums2, int n) {
         int p1 = 0, p2 = 0;
         int[] sorted = new int[m + n];
         int cur;
@@ -100,6 +101,24 @@ public class S088 {
         }
         for (int i = 0; i != m + n; ++i) {
             nums1[i] = sorted[i];
+        }
+    }
+
+    public static void merge3(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1, p2 = n - 1;
+        int tail = m + n - 1;
+        int cur;
+        while (p1 >= 0 || p2 >= 0) {
+            if (p1 == -1) {
+                cur = nums2[p2--];
+            } else if (p2 == -1) {
+                cur = nums1[p1--];
+            } else if (nums1[p1] > nums2[p2]) {
+                cur = nums1[p1--];
+            } else {
+                cur = nums2[p2--];
+            }
+            nums1[tail--] = cur;
         }
     }
 }
