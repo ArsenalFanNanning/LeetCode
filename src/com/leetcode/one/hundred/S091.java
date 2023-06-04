5c0b1a742bd04245c6bbee6c16636b444b2b00a4
Arsenal <funing.ma.china@gmail.com>
2023-06-04 15:38:12 +0800

    091 解码方法

--- src/com/leetcode/one/hundred/S091.java
+++ src/com/leetcode/one/hundred/S091.java
@@ -60,4 +60,24 @@ package com.leetcode.one.hundred;
  * s contains only digits and may contain leading zero(s).
  */
 public class S091 {
+    public static void main(String[] args) {
+        String s = "226";
+        System.out.println(numDecodings(s));
+    }
+
+    public static int numDecodings(String s) {
+        int n = s.length();
+        int[] f = new int[n + 1];
+        f[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            if (s.charAt(i - 1) != '0') {
+                f[i] += f[i - 1];
+            }
+            if (i > 1 && s.charAt(i - 2) != '0' && ((s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0') <= 26)) {
+                f[i] += f[i - 2];
+            }
+        }
+        return f[n];
+    }
+
 }
