2cc3cb6d9f3e4b907d94f977ca050873bb471155
Arsenal <funing.ma.china@gmail.com>
2023-06-08 22:42:38 +0800

    092 反转链表 II

--- src/com/leetcode/one/hundred/S092.java
+++ src/com/leetcode/one/hundred/S092.java
@@ -35,4 +35,70 @@ package com.leetcode.one.hundred;
  * Follow up: Could you do it in one pass?
  */
 public class S092 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode l12 = new ListNode(2);
+        ListNode l13 = new ListNode(3);
+        ListNode l14 = new ListNode(4);
+        ListNode l15 = new ListNode(5);
+        head.next = l12;
+        l12.next = l13;
+        l13.next = l14;
+        l14.next = l15;
+        int left = 2;
+        int right = 4;
+
+        ListNode listNode = reverseBetween(head, left, right);
+        System.out.println(listNode);
+    }
+
+
+    public static ListNode reverseBetween(ListNode head, int left, int right) {
+        // 因为头节点有可能发生变化，使用虚拟头节点可以避免复杂的分类讨论
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+
+        ListNode pre = dummyNode;
+        // 第 1 步：从虚拟头节点走 left - 1 步，来到 left 节点的前一个节点
+        // 建议写在 for 循环里，语义清晰
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+
+        // 第 2 步：从 pre 再走 right - left + 1 步，来到 right 节点
+        ListNode rightNode = pre;
+        for (int i = 0; i < right - left + 1; i++) {
+            rightNode = rightNode.next;
+        }
+
+        // 第 3 步：切断出一个子链表（截取链表）
+        ListNode leftNode = pre.next;
+        ListNode curr = rightNode.next;
+
+        // 注意：切断链接
+        pre.next = null;
+        rightNode.next = null;
+
+        // 第 4 步：同第 206 题，反转链表的子区间
+        reverseLinkedList(leftNode);
+
+        // 第 5 步：接回到原来的链表中
+        pre.next = rightNode;
+        leftNode.next = curr;
+        return dummyNode.next;
+    }
+
+    private static void reverseLinkedList(ListNode head) {
+        // 也可以使用递归反转一个链表
+        ListNode pre = null;
+        ListNode cur = head;
+
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+    }
+
 }
