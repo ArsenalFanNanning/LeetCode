61bd246997357a25fbbc493d21b112dc71f85377
Arsenal <funing.ma.china@gmail.com>
2023-06-09 00:05:25 +0800

    092 反转链表 II

--- src/com/leetcode/one/hundred/S092.java
+++ src/com/leetcode/one/hundred/S092.java
@@ -48,7 +48,8 @@ public class S092 {
         int left = 2;
         int right = 4;
 
-        ListNode listNode = reverseBetween(head, left, right);
+        //ListNode listNode = reverseBetween(head, left, right);
+        ListNode listNode = reverseBetweenOpt(head, left, right);
         System.out.println(listNode);
     }
 
@@ -100,5 +101,25 @@ public class S092 {
             cur = next;
         }
     }
+
+    public static ListNode reverseBetweenOpt(ListNode head, int left, int right) {
+        // 设置 dummyNode 是这一类问题的一般做法
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode cur = pre.next;
+        ListNode next;
+        for (int i = 0; i < right - left; i++) {
+            next = cur.next;
+            cur.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return dummyNode.next;
+    }
+
 
 }
