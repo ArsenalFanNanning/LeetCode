525b46d41d1554aee9ff2c265cf16be8362ca62b
Arsenal <funing.ma.china@gmail.com>
2023-06-17 00:03:18 +0800

    094 二叉树的中序遍历

--- src/com/leetcode/one/hundred/S094.java
+++ src/com/leetcode/one/hundred/S094.java
@@ -1,6 +1,8 @@
 package com.leetcode.one.hundred;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -47,11 +49,12 @@ public class S094 {
         root.right = node2;
         node2.right = node3;
 
-        List<Integer> list = inorderTraversal(root);
+        //List<Integer> list = inorderTraversal1(root);
+        List<Integer> list = inorderTraversal2(root);
         System.out.println(list);
     }
 
-    public static List<Integer> inorderTraversal(TreeNode root) {
+    public static List<Integer> inorderTraversal1(TreeNode root) {
         List<Integer> res = new ArrayList<Integer>();
         inorder(root, res);
         return res;
@@ -64,6 +67,21 @@ public class S094 {
         inorder(root.left, res);
         res.add(root.val);
         inorder(root.right, res);
+    }
+
+    public static List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        Deque<TreeNode> stk = new LinkedList<TreeNode>();
+        while (root != null || !stk.isEmpty()) {
+            while (root != null) {
+                stk.push(root);
+                root = root.left;
+            }
+            root = stk.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
     }
 
 }
