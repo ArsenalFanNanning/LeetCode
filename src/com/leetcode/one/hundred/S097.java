bfa34ac2cfdcb123bc3dddf2d709d4706bcd52f2
Arsenal <funing.ma.china@gmail.com>
2023-06-29 22:57:31 +0800

    097 不交错字符串

--- src/com/leetcode/one/hundred/S097.java
+++ src/com/leetcode/one/hundred/S097.java
@@ -61,6 +61,7 @@ public class S097 {
         String s2 = "dbbca";
         String s3 = "aadbbcbcac";
         System.out.println(isInterleave(s1, s2, s3));
+        System.out.println(isInterleaveOpt(s1, s2, s3));
     }
 
     public static boolean isInterleave(String s1, String s2, String s3) {
@@ -86,6 +87,31 @@ public class S097 {
         }
 
         return f[n][m];
+    }
+
+    public static boolean isInterleaveOpt(String s1, String s2, String s3) {
+        int n = s1.length(), m = s2.length(), t = s3.length();
+
+        if (n + m != t) {
+            return false;
+        }
+
+        boolean[] f = new boolean[m + 1];
+
+        f[0] = true;
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 0; j <= m; ++j) {
+                int p = i + j - 1;
+                if (i > 0) {
+                    f[j] = f[j] && s1.charAt(i - 1) == s3.charAt(p);
+                }
+                if (j > 0) {
+                    f[j] = f[j] || (f[j - 1] && s2.charAt(j - 1) == s3.charAt(p));
+                }
+            }
+        }
+
+        return f[m];
     }
 
 }
