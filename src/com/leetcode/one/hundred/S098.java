8a9f6112be9b45fa92455a2f99404de773be942c
Arsenal <funing.ma.china@gmail.com>
2023-07-03 22:33:36 +0800

    098 验证二叉搜索树

--- src/com/leetcode/one/hundred/S098.java
+++ src/com/leetcode/one/hundred/S098.java
@@ -1,5 +1,8 @@
 package com.leetcode.one.hundred;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @Author: Arsenal
  * @Date: 2023-06-30 22:43
@@ -48,7 +51,8 @@ public class S098 {
         root.right = node2;
         node2.right = node3;
 
-        System.out.println(isValidBST(root));
+        //System.out.println(isValidBST(root));
+        System.out.println(isValidBSTOpt(root));
     }
 
 
@@ -64,6 +68,27 @@ public class S098 {
             return false;
         }
         return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
+    }
+
+
+    public static boolean isValidBSTOpt(TreeNode root) {
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        double inorder = -Double.MAX_VALUE;
+
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            // 如果中序遍历得到的节点的值小于等于前一个 inorder，说明不是二叉搜索树
+            if (root.val <= inorder) {
+                return false;
+            }
+            inorder = root.val;
+            root = root.right;
+        }
+        return true;
     }
 
 }
