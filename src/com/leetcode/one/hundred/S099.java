20e0c5ddfb275a8d395ae00f92502b1fa862ad5c
Arsenal <funing.ma.china@gmail.com>
2023-07-08 00:01:52 +0800

    099 恢复二叉搜索树

--- src/com/leetcode/one/hundred/S099.java
+++ src/com/leetcode/one/hundred/S099.java
@@ -49,7 +49,8 @@ public class S099 {
         node2.right = node3;
 
         //recoverTree1(root);
-        recoverTree2(root);
+        //recoverTree2(root);
+        recoverTree3(root);
     }
 
     public static void recoverTree1(TreeNode root) {
@@ -129,4 +130,50 @@ public class S099 {
         x.val = y.val;
         y.val = tmp;
     }
+
+    public static void recoverTree3(TreeNode root) {
+        TreeNode x = null, y = null, pred = null, predecessor = null;
+
+        while (root != null) {
+            if (root.left != null) {
+                // predecessor 节点就是当前 root 节点向左走一步，然后一直向右走至无法走为止
+                predecessor = root.left;
+                while (predecessor.right != null && predecessor.right != root) {
+                    predecessor = predecessor.right;
+                }
+
+                // 让 predecessor 的右指针指向 root，继续遍历左子树
+                if (predecessor.right == null) {
+                    predecessor.right = root;
+                    root = root.left;
+                }
+                // 说明左子树已经访问完了，我们需要断开链接
+                else {
+                    if (pred != null && root.val < pred.val) {
+                        y = root;
+                        if (x == null) {
+                            x = pred;
+                        }
+                    }
+                    pred = root;
+
+                    predecessor.right = null;
+                    root = root.right;
+                }
+            }
+            // 如果没有左孩子，则直接访问右孩子
+            else {
+                if (pred != null && root.val < pred.val) {
+                    y = root;
+                    if (x == null) {
+                        x = pred;
+                    }
+                }
+                pred = root;
+                root = root.right;
+            }
+        }
+        swap(x, y);
+    }
+
 }
