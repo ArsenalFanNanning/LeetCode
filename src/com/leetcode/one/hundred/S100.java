301e6a979070d81359f422f4cf841c1ebc454a7f
Arsenal <funing.ma.china@gmail.com>
2023-07-10 23:12:42 +0800

    100 相同的树

--- src/com/leetcode/one/hundred/S100.java
+++ src/com/leetcode/one/hundred/S100.java
@@ -18,6 +18,21 @@ package com.leetcode.one.hundred;
  * 提示：
  * 两棵树上的节点数目都在范围 [0, 100] 内
  * -104 <= Node.val <= 104
+ * 100. Same Tree
+ * Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+ * Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+ * Example 1:
+ * Input: p = [1,2,3], q = [1,2,3]
+ * Output: true
+ * Example 2:
+ * Input: p = [1,2], q = [1,null,2]
+ * Output: false
+ * Example 3:
+ * Input: p = [1,2,1], q = [1,1,2]
+ * Output: false
+ * Constraints:
+ * The number of nodes in both trees is in the range [0, 100].
+ * -104 <= Node.val <= 104
  */
 public class S100 {
 }
