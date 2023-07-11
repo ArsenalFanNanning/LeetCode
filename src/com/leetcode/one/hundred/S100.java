38307e81af337dbd8c501b90773495c332b578d9
Arsenal <funing.ma.china@gmail.com>
2023-07-11 23:24:08 +0800

    100 相同的树

--- src/com/leetcode/one/hundred/S100.java
+++ src/com/leetcode/one/hundred/S100.java
@@ -35,4 +35,33 @@ package com.leetcode.one.hundred;
  * -104 <= Node.val <= 104
  */
 public class S100 {
+    public static void main(String[] args) {
+        TreeNode p = new TreeNode();
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node3 = new TreeNode(3);
+        p.left = node1;
+        p.right = node3;
+
+
+        TreeNode q = new TreeNode();
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node4 = new TreeNode(4);
+        q.left = node2;
+        q.right = node4;
+
+        System.out.println(isSameTree(p, q));
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+
 }
