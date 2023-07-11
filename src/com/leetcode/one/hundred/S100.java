a872ecb0f041aae9e23fbd0ed918e42957705f6b
Arsenal <funing.ma.china@gmail.com>
2023-07-12 00:04:29 +0800

    100 相同的树

--- src/com/leetcode/one/hundred/S100.java
+++ src/com/leetcode/one/hundred/S100.java
@@ -1,5 +1,8 @@
 package com.leetcode.one.hundred;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @Author: Arsenal
  * @Date: 2023-07-09 09:48
@@ -49,7 +52,8 @@ public class S100 {
         q.left = node2;
         q.right = node4;
 
-        System.out.println(isSameTree(p, q));
+        //System.out.println(isSameTree(p, q));
+        System.out.println(isSameTreeOpt(p, q));
     }
 
     public static boolean isSameTree(TreeNode p, TreeNode q) {
@@ -62,6 +66,45 @@ public class S100 {
         } else {
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         }
+    }
+
+    public static boolean isSameTreeOpt(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        }
+        Queue<TreeNode> queue1 = new LinkedList<TreeNode>();
+        Queue<TreeNode> queue2 = new LinkedList<TreeNode>();
+        queue1.offer(p);
+        queue2.offer(q);
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            TreeNode node1 = queue1.poll();
+            TreeNode node2 = queue2.poll();
+            if (node1.val != node2.val) {
+                return false;
+            }
+            TreeNode left1 = node1.left, right1 = node1.right, left2 = node2.left, right2 = node2.right;
+            if (left1 == null ^ left2 == null) {
+                return false;
+            }
+            if (right1 == null ^ right2 == null) {
+                return false;
+            }
+            if (left1 != null) {
+                queue1.offer(left1);
+            }
+            if (right1 != null) {
+                queue1.offer(right1);
+            }
+            if (left2 != null) {
+                queue2.offer(left2);
+            }
+            if (right2 != null) {
+                queue2.offer(right2);
+            }
+        }
+        return queue1.isEmpty() && queue2.isEmpty();
     }
 
 }
