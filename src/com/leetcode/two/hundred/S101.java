94dc41a315e87390f5568b8bcb713a514eee2bd7
Arsenal <funing.ma.china@gmail.com>
2023-07-13 23:10:23 +0800

    101 对称二叉树

---
+++ src/com/leetcode/two/hundred/S101.java
@@ -0,0 +1,19 @@
+package com.leetcode.two.hundred;
+
+/**
+ * @Author: Arsenal
+ * @Date: 2023-07-13 23:08
+ * @Description: 对称二叉树 -简单
+ * 给你一个二叉树的根节点 root ， 检查它是否轴对称。
+ * 示例 1：
+ * 输入：root = [1,2,2,3,4,4,3]
+ * 输出：true
+ * 示例 2：
+ * 输入：root = [1,2,2,null,3,null,3]
+ * 输出：false
+ * 提示：
+ * 树中节点数目在范围 [1, 1000] 内
+ * -100 <= Node.val <= 100
+ */
+public class S101 {
+}
