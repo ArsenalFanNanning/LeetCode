cda95698168f26b7bcf07332ca045d25a2c06d9e
Arsenal <funing.ma.china@gmail.com>
2023-07-19 23:39:56 +0800

    102 二叉树的层序遍历

--- src/com/leetcode/two/hundred/S102.java
+++ src/com/leetcode/two/hundred/S102.java
@@ -1,5 +1,12 @@
 package com.leetcode.two.hundred;
 
+import com.leetcode.one.hundred.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * @Author: Arsenal
  * @Date: 2023-07-17 23:14
@@ -34,4 +41,44 @@ package com.leetcode.two.hundred;
  * -1000 <= Node.val <= 1000
  */
 public class S102 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode();
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node3 = new TreeNode(3);
+        root.left = node1;
+        root.right = node3;
+
+        List<List<Integer>> lists = levelOrder(root);
+        for (List<Integer> list : lists) {
+            System.out.println(list);
+        }
+    }
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return ret;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<Integer>();
+            int currentLevelSize = queue.size();
+            for (int i = 1; i <= currentLevelSize; ++i) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            ret.add(level);
+        }
+
+        return ret;
+    }
+
 }
