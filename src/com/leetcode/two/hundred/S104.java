a14bd0ff061868be0694de32254013ac1b20dc0e
Arsenal <funing.ma.china@gmail.com>
2023-07-25 00:04:07 +0800

    104 二叉树展开为链表

--- src/com/leetcode/two/hundred/S104.java
+++ src/com/leetcode/two/hundred/S104.java
@@ -1,5 +1,10 @@
 package com.leetcode.two.hundred;
 
+import com.leetcode.one.hundred.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: Arsenal
  * @Date: 2023-07-23 08:17
@@ -40,4 +45,32 @@ package com.leetcode.two.hundred;
  * Follow up: Can you flatten the tree in-place (with O(1) extra space)?
  */
 public class S104 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode();
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node3 = new TreeNode(3);
+        root.left = node1;
+        root.right = node3;
+
+        flatten(root);
+    }
+
+    public static void flatten(TreeNode root) {
+        List<TreeNode> list = new ArrayList<TreeNode>();
+        preorderTraversal(root, list);
+        int size = list.size();
+        for (int i = 1; i < size; i++) {
+            TreeNode prev = list.get(i - 1), curr = list.get(i);
+            prev.left = null;
+            prev.right = curr;
+        }
+    }
+
+    public static void preorderTraversal(TreeNode root, List<TreeNode> list) {
+        if (root != null) {
+            list.add(root);
+            preorderTraversal(root.left, list);
+            preorderTraversal(root.right, list);
+        }
+    }
 }
