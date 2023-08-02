9e276ce93bee02011491b65951df755c8b122f13
Arsenal <funing.ma.china@gmail.com>
2023-08-02 23:38:52 +0800

    105 从前序与中序遍历序列构造二叉树

--- src/com/leetcode/two/hundred/S106.java
+++ src/com/leetcode/two/hundred/S106.java
@@ -20,6 +20,23 @@ package com.leetcode.two.hundred;
  * postorder 中每一个值都在 inorder 中
  * inorder 保证是树的中序遍历
  * postorder 保证是树的后序遍历
+ * 106. Construct Binary Tree from Inorder and Postorder Traversal
+ * Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and
+ * postorder is the postorder traversal of the same tree, construct and return the binary tree.
+ * Example 1:
+ * Input: inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
+ * Output: [3,9,20,null,null,15,7]
+ * Example 2:
+ * Input: inorder = [-1], postorder = [-1]
+ * Output: [-1]
+ * Constraints:
+ * 1 <= inorder.length <= 3000
+ * postorder.length == inorder.length
+ * -3000 <= inorder[i], postorder[i] <= 3000
+ * inorder and postorder consist of unique values.
+ * Each value of postorder also appears in inorder.
+ * inorder is guaranteed to be the inorder traversal of the tree.
+ * postorder is guaranteed to be the postorder traversal of the tree.
  */
 public class S106 {
 }
