96998da7ccdf08618f278a6af20b71836138c5de
Arsenal <funing.ma.china@gmail.com>
2023-08-03 00:04:12 +0800

    105 从前序与中序遍历序列构造二叉树

--- src/com/leetcode/two/hundred/S106.java
+++ src/com/leetcode/two/hundred/S106.java
@@ -1,5 +1,10 @@
 package com.leetcode.two.hundred;
 
+import com.leetcode.one.hundred.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Author: Arsenal
  * @Date: 2023-07-31 22:50
@@ -39,4 +44,53 @@ package com.leetcode.two.hundred;
  * postorder is guaranteed to be the postorder traversal of the tree.
  */
 public class S106 {
+    public static void main(String[] args) {
+        int[] inorder = {9, 3, 15, 20, 7};
+        int[] postorder = {9, 15, 7, 20, 3};
+
+        TreeNode treeNode = buildTree(inorder, postorder);
+        System.out.println(treeNode);
+    }
+
+    static int post_idx;
+    static int[] postorder;
+    static int[] inorder;
+    static Map<Integer, Integer> idx_map = new HashMap<Integer, Integer>();
+
+    public static TreeNode helper(int in_left, int in_right) {
+        // 如果这里没有节点构造二叉树了，就结束
+        if (in_left > in_right) {
+            return null;
+        }
+
+        // 选择 post_idx 位置的元素作为当前子树根节点
+        int root_val = postorder[post_idx];
+        TreeNode root = new TreeNode(root_val);
+
+        // 根据 root 所在位置分成左右两棵子树
+        int index = idx_map.get(root_val);
+
+        // 下标减一
+        post_idx--;
+        // 构造右子树
+        root.right = helper(index + 1, in_right);
+        // 构造左子树
+        root.left = helper(in_left, index - 1);
+        return root;
+    }
+
+    public static TreeNode buildTree(int[] inorder, int[] postorder) {
+        postorder = postorder;
+        inorder = inorder;
+        // 从后序遍历的最后一个元素开始
+        post_idx = postorder.length - 1;
+
+        // 建立（元素，下标）键值对的哈希表
+        int idx = 0;
+        for (Integer val : inorder) {
+            idx_map.put(val, idx++);
+        }
+
+        return helper(0, inorder.length - 1);
+    }
 }
