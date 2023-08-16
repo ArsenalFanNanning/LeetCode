285aa5b61132771ec5285dd9bf5be05c335bdc32
Arsenal <funing.ma.china@gmail.com>
2023-08-16 23:34:26 +0800

    109 有序链表转换二叉搜索树

--- src/com/leetcode/two/hundred/S109.java
+++ src/com/leetcode/two/hundred/S109.java
@@ -16,6 +16,20 @@ package com.leetcode.two.hundred;
  * 提示:
  * head 中的节点数在[0, 2 * 104] 范围内
  * -105 <= Node.val <= 105
+ * 109. Convert Sorted List to Binary Search Tree
+ * Given the head of a singly linked list where elements are sorted in ascending order, convert it to a
+ * height-balanced
+ * binary search tree.
+ * Example 1:
+ * Input: head = [-10,-3,0,5,9]
+ * Output: [0,-3,9,-10,null,5]
+ * Explanation: One possible answer is [0,-3,9,-10,null,5], which represents the shown height balanced BST.
+ * Example 2:
+ * Input: head = []
+ * Output: []
+ * Constraints:
+ * The number of nodes in head is in the range [0, 2 * 104].
+ * -105 <= Node.val <= 105
  */
 public class S109 {
 }
