7031c658d2f4ff1c938d1d6c68935907353d59cf
Arsenal <funing.ma.china@gmail.com>
2023-08-22 00:01:13 +0800

    110 平衡二叉树

--- src/com/leetcode/two/hundred/S110.java
+++ src/com/leetcode/two/hundred/S110.java
@@ -19,6 +19,21 @@ package com.leetcode.two.hundred;
  * 提示：
  * 树中的节点数在范围 [0, 5000] 内
  * -104 <= Node.val <= 104
+ * 110. Balanced Binary Tree
+ * Given a binary tree, determine if it is
+ * height-balanced
+ * Example 1:
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: true
+ * Example 2:
+ * Input: root = [1,2,2,3,3,null,null,4,4]
+ * Output: false
+ * Example 3:
+ * Input: root = []
+ * Output: true
+ * Constraints:
+ * The number of nodes in the tree is in the range [0, 5000].
+ * -104 <= Node.val <= 104
  */
 public class S110 {
 }
