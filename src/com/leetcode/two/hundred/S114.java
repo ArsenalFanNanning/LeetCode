398dff0024023d4881e79d93e40bbf75094b01fb
Arsenal <funing.ma.china@gmail.com>
2023-09-11 00:03:44 +0800

    114 二叉树展开为链表

---
+++ src/com/leetcode/two/hundred/S114.java
@@ -0,0 +1,121 @@
+package com.leetcode.two.hundred;
+
+import com.leetcode.one.hundred.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Author: Arsenal
+ * @Date: 2023-09-02 23:47
+ * @Description: 二叉树展开为链表 -中等
+ * 给你二叉树的根结点 root ，请你将它展开为一个单链表：
+ * 展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+ * 展开后的单链表应该与二叉树 先序遍历 顺序相同。
+ * 示例 1：
+ * 输入：root = [1,2,5,3,4,null,6]
+ * 输出：[1,null,2,null,3,null,4,null,5,null,6]
+ * 示例 2：
+ * 输入：root = []
+ * 输出：[]
+ * 示例 3：
+ * 输入：root = [0]
+ * 输出：[0]
+ * 提示：
+ * 树中结点数在范围 [0, 2000] 内
+ * -100 <= Node.val <= 100
+ * 114. Flatten Binary Tree to Linked List
+ * Given the root of a binary tree, flatten the tree into a "linked list":
+ * The "linked list" should use the same TreeNode class where the right
+ * child pointer points to the next node in the list and the left child pointer is always null.
+ * The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+ * Example 1:
+ * Input: root = [1,2,5,3,4,null,6]
+ * Output: [1,null,2,null,3,null,4,null,5,null,6]
+ * Example 2:
+ * Input: root = []
+ * Output: []
+ * Example 3:
+ * Input: root = [0]
+ * Output: [0]
+ * Constraints:
+ * The number of nodes in the tree is in the range [0, 2000].
+ * -100 <= Node.val <= 100
+ * Follow up: Can you flatten the tree in-place (with O(1) extra space)?
+ */
+public class S114 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode();
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node3 = new TreeNode(3);
+        root.left = node1;
+        root.right = node3;
+
+        // flatten1(root);
+        // flatten2(root);
+        flatten3(root);
+        System.out.println(root);
+    }
+
+    public static void flatten1(TreeNode root) {
+        List<TreeNode> list = new ArrayList<>();
+        preorderTraversal(root, list);
+        int size = list.size();
+        for (int i = 1; i < size; i++) {
+            TreeNode prev = list.get(i - 1), curr = list.get(i);
+            prev.right = curr;
+            prev.left = null;
+        }
+    }
+
+    public static void preorderTraversal(TreeNode root, List<TreeNode> list) {
+        if (root != null) {
+            list.add(root);
+            preorderTraversal(root.left, list);
+            preorderTraversal(root.right, list);
+        }
+    }
+
+    public static void flatten2(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        stack.push(root);
+        TreeNode prev = null;
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+            if (prev != null) {
+                prev.left = null;
+                prev.right = curr;
+            }
+            TreeNode left = curr.left, right = curr.right;
+            if (right != null) {
+                stack.push(right);
+            }
+            if (left != null) {
+                stack.push(left);
+            }
+            prev = curr;
+        }
+    }
+
+    public static void flatten3(TreeNode root) {
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode next = curr.left;
+                TreeNode predecessor = next;
+                while (predecessor.right != null) {
+                    predecessor = predecessor.right;
+                }
+                predecessor.right = curr.right;
+                curr.left = null;
+                curr.right = next;
+            }
+            curr = curr.right;
+        }
+    }
+}
