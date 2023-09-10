5712bc62a0c4914292bfcd5f44cc75380c08fd40
Arsenal <funing.ma.china@gmail.com>
2023-09-10 23:49:03 +0800

    113 路径总和 II

--- src/com/leetcode/two/hundred/S113.java
+++ src/com/leetcode/two/hundred/S113.java
@@ -2,9 +2,7 @@ package com.leetcode.two.hundred;
 
 import com.leetcode.one.hundred.TreeNode;
 
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @Author: Arsenal
@@ -58,7 +56,10 @@ public class S113 {
 
         int targetSum = 3;
 
-        List<List<Integer>> lists = pathSum(root, targetSum);
+        // List<List<Integer>> lists = pathSum(root, targetSum);
+        List<List<Integer>> lists = pathSumOpt(root, targetSum);
+
+
         for (List<Integer> list : lists) {
             System.out.println(list);
         }
@@ -86,4 +87,53 @@ public class S113 {
         dfs(root.right, targetSum);
         path.pollLast();
     }
+
+
+    static Map<TreeNode, TreeNode> map = new HashMap<TreeNode, TreeNode>();
+
+    public static List<List<Integer>> pathSumOpt(TreeNode root, int targetSum) {
+        if (root == null) {
+            return ret;
+        }
+
+        Queue<TreeNode> queueNode = new LinkedList<TreeNode>();
+        Queue<Integer> queueSum = new LinkedList<Integer>();
+        queueNode.offer(root);
+        queueSum.offer(0);
+
+        while (!queueNode.isEmpty()) {
+            TreeNode node = queueNode.poll();
+            int rec = queueSum.poll() + node.val;
+
+            if (node.left == null && node.right == null) {
+                if (rec == targetSum) {
+                    getPath(node);
+                }
+            } else {
+                if (node.left != null) {
+                    map.put(node.left, node);
+                    queueNode.offer(node.left);
+                    queueSum.offer(rec);
+                }
+                if (node.right != null) {
+                    map.put(node.right, node);
+                    queueNode.offer(node.right);
+                    queueSum.offer(rec);
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    public static void getPath(TreeNode node) {
+        List<Integer> temp = new LinkedList<Integer>();
+        while (node != null) {
+            temp.add(node.val);
+            node = map.get(node);
+        }
+        Collections.reverse(temp);
+        ret.add(new LinkedList<Integer>(temp));
+    }
+
 }
